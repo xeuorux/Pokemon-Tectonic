849a8ab1ce4b9034207f3af7f8cb8f0529d95a82
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2021-07-27 11:08:03 -0700

    Zoo tells you which map the pokemon was placed in, new debug event for checking zoo design progress

--- Plugins/Zoo/Zoo.rb
+++ Plugins/Zoo/Zoo.rb
@@ -6,46 +6,56 @@ def forEachZooMap()
 end
 
 def canBeSentToZoo(pkmn)
-	return !isInZoo(pkmn.species)
+	return !donatedToZoo?(pkmn.species)
 end
 
-def isInZoo(species)
+def donatedToZoo?(species)
 	speciesName = GameData::Species.get(species).id.to_s
+	return checkFor(speciesName,true)
+end
 
-	isInZoo = false
+def eventExistsFor?(species)
+	speciesName = GameData::Species.get(species).id.to_s
+	return checkFor(speciesName,false)
+end
+
+def checkFor(speciesName,careAboutEnabled)
 	forEachZooMap do |map|
 		map.events.each_value { |event|
-			if event.name.include?(speciesName) && $game_self_switches[[map.map_id, event.id, "A"]]
-				isInZoo = true
+			if event.name.include?(speciesName) && (!careAboutEnabled || $game_self_switches[[map.map_id, event.id, "A"]])
+				return true
 			end
 		}
 	end
-	return isInZoo
+	return false
 end
 
 def sendToZoo()
-	return unless pbConfirmMessageSerious(_INTL("Are you sure you want to send {1} to the Zoo?",pbGet(3)))
+	pokemonName = pbGet(3)
+	return unless pbConfirmMessageSerious(_INTL("Are you sure you want to send {1} to the Zoo?",pokemonName))
 	
 	species = pbGetPokemon(1).species
 	speciesName = GameData::Species.get(species).id.to_s
 	
-	successfulSet = false
+	placementMap = nil
 	forEachZooMap do |map|
 		map.events.each_value { |event|
 			if event.name.include?(speciesName)
 				pbSetSelfSwitch(event.id,"A",true,map.map_id)
-				successfuLSet = true
+				placementMap = (pbGetMessage(MessageTypes::MapNames,map.map_id) rescue nil) || "???" if !placementMap
 			end
 		}
 	end
 	
-	if !successfulSet
-		pbMessage("Unable to find that species in the zoo!")
+	if !placementMap
+		pbMessage("ERROR: Unable to find that species in the zoo!")
 		return
 	end
 	
 	$Trainer.party[pbGet(1)] = nil
 	$Trainer.party.compact!
+	
+	pbMessage("#{pokemonName} was placed in #{placementMap}!")
 	
 	refreshFollow()
 end
@@ -68,6 +78,25 @@ def debugTurnOffAll()
 			end
 		}
 	end
+end
+
+def debugCheckMissingSpecies()
+	totalPresent = 0
+	totalMissing = 0
+	GameData::Species.each do |species_data|
+		next if species_data.form != 0
+		if !eventExistsFor?(species_data.species)
+			echo("No Zoo event exists for species: #{species_data.real_name}\n")
+			totalMissing += 1
+		else
+			totalPresent += 1
+		end
+	end
+	pbMessage("#{totalPresent} species have an event.")
+	pbMessage("#{totalMissing} species do NOT have an event.")
+	completion = totalPresent.to_f / (totalPresent + totalMissing).to_f
+	completion = (completion*10000).floor / 100.0
+	pbMessage("The Zoo is #{completion} percent complete.")
 end
 
 # More likely to be shiny
