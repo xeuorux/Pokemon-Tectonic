9d4b945ed9d08c4c3eea8beeae175bbe36591294
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2021-07-27 08:35:57 -0700

    Turn off trainer dialogue subscriber debug messages

--- Plugins/InBattleDialogue/Subscribers.rb
+++ Plugins/InBattleDialogue/Subscribers.rb
@@ -3,7 +3,7 @@
 #===============================================================================
 PokeBattle_AI::TrainerChoseMoveDialogue.add(:TEST,
   proc { |policy,battler,move,target,trainername,dialogue_array|
-	dialogue_array.push("I chose to use a move!") if $DEBUG
+	# Push to the dialogue_array based on conditions
     next dialogue_array
   }
 )
@@ -14,7 +14,6 @@ PokeBattle_AI::TrainerChoseMoveDialogue.add(:TEST,
 PokeBattle_AI::PlayerChoseMoveDialogue.add(:TEST,
   proc { |policy,battler,move,target,dialogue_array|
 	# Push to the dialogue_array based on conditions
-	dialogue_array.push("You chose to use a move!") if $DEBUG
     next dialogue_array
   }
 )
@@ -25,7 +24,6 @@ PokeBattle_AI::PlayerChoseMoveDialogue.add(:TEST,
 PokeBattle_AI::TrainerPokemonFaintedDialogue.add(:TEST,
   proc { |policy,battler,dialogue_array|
 	# Push to the dialogue_array based on conditions
-	dialogue_array.push("My Pokemon fainted!") if $DEBUG
     next dialogue_array
   }
 )
@@ -36,7 +34,6 @@ PokeBattle_AI::TrainerPokemonFaintedDialogue.add(:TEST,
 PokeBattle_AI::PlayerPokemonFaintedDialogue.add(:TEST,
   proc { |policy,battler,dialogue_array|
 	# Push to the dialogue_array based on conditions
-	dialogue_array.push("Your Pokemon fainted!") if $DEBUG
     next dialogue_array
   }
 )
