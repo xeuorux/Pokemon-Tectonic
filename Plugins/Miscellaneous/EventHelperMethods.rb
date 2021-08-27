8a1b460c0cf3ba8e7533f4bbeec6ac16223fcc91
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2021-08-26 23:17:02 -0700

    Some new moves given out, teleport tiles

--- Plugins/Miscellaneous/EventHelperMethods.rb
+++ Plugins/Miscellaneous/EventHelperMethods.rb
@@ -312,15 +312,19 @@ def moveBackAndForth(length,initialDirection=Right,transverseLength=0,clockwise=
 	self.force_move_route(new_move_route)
 end
 
+def getNewMoveRoute()
+	new_move_route = RPG::MoveRoute.new
+	new_move_route.repeat    = false
+	new_move_route.skippable = false
+	new_move_route.list.clear
+	return new_move_route
+end
 
 
 def changeOpacityOverTime(opacityTarget,speed)
 	currentOpacity = self.opacity
 	
-	new_move_route = RPG::MoveRoute.new
-	new_move_route.repeat    = false
-	new_move_route.skippable = false
-	new_move_route.list.clear
+	new_move_route = getNewMoveRoute()
 	
 	calculatedOpacity = currentOpacity
 	targetReached = false
@@ -478,4 +482,39 @@ def stowFollowerIfActive()
 		pbToggleFollowingPokemon("off",true)
 		pbWait(Graphics.frame_rate)
 	end
+end
+
+def teleportLeaveAnimation
+	stowFollowerIfActive()
+	player = get_player
+
+	new_move_route = getNewMoveRoute()
+	frame = 0
+	while frame <= 16
+		opac = 255 * (1-frame/16.0)
+		new_move_route.list.push(RPG::MoveCommand.new(PBMoveRoute::Opacity,[opac]))
+		new_move_route.list.push(RPG::MoveCommand.new(PBMoveRoute::PlaySE,[RPG::AudioFile.new("Player jump"),120])) if frame % 4 == 0
+		new_move_route.list.push(RPG::MoveCommand.new(PBMoveRoute::Wait,[1]))
+		frame += 1
+	end
+
+	new_move_route.list.push(RPG::MoveCommand.new(PBMoveRoute::Wait,[5]))
+	new_move_route.list.push(RPG::MoveCommand.new(0)) # End of move route
+	
+	get_player.force_move_route(new_move_route)
+end
+
+def teleportArriveAnimation
+	new_move_route = getNewMoveRoute()
+	frame = 0
+	while frame <= 16
+		opac = 255 * (frame/16.0)
+		new_move_route.list.push(RPG::MoveCommand.new(PBMoveRoute::Opacity,[opac]))
+		new_move_route.list.push(RPG::MoveCommand.new(PBMoveRoute::PlaySE,[RPG::AudioFile.new("Player jump"),120])) if frame % 4 == 0
+		new_move_route.list.push(RPG::MoveCommand.new(PBMoveRoute::Wait,[1]))
+		frame += 1
+	end
+	new_move_route.list.push(RPG::MoveCommand.new(0)) # End of move route
+	
+	get_player.force_move_route(new_move_route)
 end
