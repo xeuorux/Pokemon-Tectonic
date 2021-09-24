7ba026f930fe182ca42adf61b7d3e7cf9daef0ed
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2021-09-23 20:42:05 -0700

    Debug only option to skip all black fades

--- Plugins/Miscellaneous/MiscChanges.rb
+++ Plugins/Miscellaneous/MiscChanges.rb
@@ -194,3 +194,42 @@ class Interpreter
     return true
   end
 end
+
+def pbFadeOutAndHide(sprites)
+  visiblesprites = {}
+  numFrames = (Graphics.frame_rate*0.4).floor
+  numFrames = 1 if $PokemonSystem.skip_fades == 0
+  alphaDiff = (255.0/numFrames).ceil
+  pbDeactivateWindows(sprites) {
+    for j in 0..numFrames
+      pbSetSpritesToColor(sprites,Color.new(0,0,0,j*alphaDiff))
+      (block_given?) ? yield : pbUpdateSpriteHash(sprites)
+    end
+  }
+  for i in sprites
+    next if !i[1]
+    next if pbDisposed?(i[1])
+    visiblesprites[i[0]] = true if i[1].visible
+    i[1].visible = false
+  end
+  return visiblesprites
+end
+
+def pbFadeInAndShow(sprites,visiblesprites=nil)
+  if visiblesprites
+    for i in visiblesprites
+      if i[1] && sprites[i[0]] && !pbDisposed?(sprites[i[0]])
+        sprites[i[0]].visible = true
+      end
+    end
+  end
+  numFrames = (Graphics.frame_rate*0.4).floor
+  numFrames = 1 if $PokemonSystem.skip_fades == 0
+  alphaDiff = (255.0/numFrames).ceil
+  pbDeactivateWindows(sprites) {
+    for j in 0..numFrames
+      pbSetSpritesToColor(sprites,Color.new(0,0,0,((numFrames-j)*alphaDiff)))
+      (block_given?) ? yield : pbUpdateSpriteHash(sprites)
+    end
+  }
+end
