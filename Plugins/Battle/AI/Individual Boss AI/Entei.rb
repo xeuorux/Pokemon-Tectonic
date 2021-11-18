0bbf0206a1c8cf1a302a8cbc1d9909e87c3ad941
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2021-11-18 15:28:52 -0700

    Entei fight redesign and AI change

--- Plugins/Battle/AI/Individual Boss AI/Entei.rb
+++ Plugins/Battle/AI/Individual Boss AI/Entei.rb
@@ -1,6 +1,11 @@
-PokeBattle_AI::BossSpeciesRequireMove.add(:ENTEI,
-	proc { |species,move,user,target|
-		# Always incinerate items if you can
-		next true if move.function == "0F5" && target.item && (target.item.is_berry? || target.item.is_gem?)
-	}
+PokeBattle_AI::BossSpeciesUseMoveIDIfAndOnlyIf.add([:ENTEI,:INCINERATE],
+  proc { |speciesAndMove,user,target,move|
+	next target.item && (target.item.is_berry? || target.item.is_gem?)
+  }
+)
+
+PokeBattle_AI::BossSpeciesUseMoveIDIfAndOnlyIf.add([:ENTEI,:SUNNYDAY],
+  proc { |speciesAndMove,user,target,move|
+	next user.battle.field.weather != :Sun && user.hp <= user.hptotal / 2
+  }
 )
