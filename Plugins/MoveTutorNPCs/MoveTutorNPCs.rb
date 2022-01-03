2dd2113ba5fcae87a81dfac2462ea68412c3c5ff
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-01-02 18:03:55 -0700

    Move relearner NPC script refactor, slightly less tedium

--- Plugins/MoveTutorNPCs/MoveTutorNPCs.rb
+++ Plugins/MoveTutorNPCs/MoveTutorNPCs.rb
@@ -1,3 +1,29 @@
+def moveRelearner()
+	if isTempSwitchOff?("A")
+		pbMessage(_INTL("I'm the Pokémon Move Maniac."))
+		pbMessage(_INTL("I know every single move that Pokémon learn while leveling up or evolving."))
+		pbMessage(_INTL("I can teach moves to your Pokémon -- at no cost!"))
+		setTempSwitchOn("A")
+	end
+	if pbConfirmMessage(_INTL("Do you want me to teach one of your Pokémon a move?"))
+		while true do
+			pbChoosePokemon(1,3,proc{|p|
+				p.can_relearn_move?
+			},false)
+			if $game_variables[1] < 0
+				pbMessage(_INTL("If your Pokémon need to learn a move, come to me!"))
+				break
+			elsif !pbGetPokemon(1).can_relearn_move?
+				pbMessage(_INTL("Sorry, it doesn't appear as if I have any move I can teach to your \v[3]."))
+			else
+				pbRelearnMoveScreen(pbGetPokemon(1))
+			end
+		end
+	else
+		pbMessage(_INTL("If your Pokémon need to learn a move, come to me!"))
+	end
+end
+
 class Pokemon
 	def can_relearn_move?
 		return false if egg? || shadowPokemon?
