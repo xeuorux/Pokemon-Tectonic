3fd35005086649a66f24806b62c9203d0af85f3a
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-01-09 20:16:29 -0700

    Distribute Fairy, Flying, Dragon, and Fighting gems

--- Plugins/Miscellaneous/DebugCommands.rb
+++ Plugins/Miscellaneous/DebugCommands.rb
@@ -525,20 +525,27 @@ DebugMenuCommands.register("analyzeitemdistribution", {
   "description" => _INTL("Find the locations of all item distributions."),
   "effect"      => proc { |sprites, viewport|
 	mapData = Compiler::MapData.new
+	allItemsGiven = []
     for id in mapData.mapinfos.keys.sort
 		map = mapData.getMap(id)
 		next if !map || !mapData.mapinfos[id]
 		mapName = mapData.mapinfos[id].name
 		for key in map.events.keys
-			analyze_items(id,mapName,map.events[key])
+			itemsGiven = analyze_items(id,mapName,map.events[key])
+			allItemsGiven.concat(itemsGiven)
 		end
 	end
 
+	echoln("All the items which have not a single distribution")
+	GameData::Item.each do |itemData|
+		echoln(itemData.id) if !allItemsGiven.include?(itemData.id)
+	end
   }}
 )
 
 def analyze_items(map_id,map_name,event)
-	return nil if !event || event.pages.length==0
+	return [] if !event || event.pages.length==0
+	itemsGiven = []
 	event.pages.each do |page|
 		page.list.each do |eventCommand|
 			eventCommand.parameters.each do |parameter|
@@ -549,11 +556,15 @@ def analyze_items(map_id,map_name,event)
 					eventName = event.name.gsub(",","")
 					itemCount = match[2] ? match[2] : 1
 					string = "#{map_id},#{map_name},#{event.id},#{eventName},#{itemName},#{itemCount}"
+					
+					itemsGiven.push(itemName.to_sym)
+					
 					echoln(string)
 				end
 			end
 		end
 	end
+	return itemsGiven
 end
 
  
