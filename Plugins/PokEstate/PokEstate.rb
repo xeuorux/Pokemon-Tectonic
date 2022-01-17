05bf0f688b75c2991dfb3a724f770a9c6e3075b4
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-01-17 14:21:40 -0700

    PokEstate cave

--- Plugins/PokEstate/PokEstate.rb
+++ Plugins/PokEstate/PokEstate.rb
@@ -11,7 +11,7 @@ def estate_box()
 	return $PokemonGlobal.estate_box
 end
 
-ESTATE_MAP_IDS 				= [148,150,151,152,153,154,160]
+ESTATE_MAP_IDS 				= [148,150,151,152,153,154,160,161]
 FALLBACK_MAP_ID				= 149
 ESTATE_MAP_ENTRANCES	= [[20,30,8],[31,18,4],[8,18,6]] # S, E, W
 
