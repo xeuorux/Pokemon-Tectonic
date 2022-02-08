f814bc65441b5cd87a274100fe07ad75bcbb4fb2
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-02-07 20:42:48 -0700

    Brooding fix

--- Plugins/Battle/Abilities/SpeedCalcAbilities.rb
+++ Plugins/Battle/Abilities/SpeedCalcAbilities.rb
@@ -7,7 +7,7 @@ BattleHandlers::SpeedCalcAbility.add(:QUICKTHINKING,
 BattleHandlers::SpeedCalcAbility.add(:BROODING,
   proc { |ability,battler,mult|
 	dragonCount = 0
-	user.battle.eachInTeamFromBattlerIndex(battler.index) do |pkmn,i|
+	battler.battle.eachInTeamFromBattlerIndex(battler.index) do |pkmn,i|
 		dragonCount += 1 if pkmn.pbHasType?(:DRAGON)
 	end
 	next mult * (1.0 + dragonCount * 0.05) 
