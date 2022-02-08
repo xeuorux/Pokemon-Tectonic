e8b5a6155af1c76d20383ddea1cb85bdcd47776f
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-02-07 20:05:37 -0700

    Aid kit echolns for debugging

--- Plugins/Items/NewItems.rb
+++ Plugins/Items/NewItems.rb
@@ -207,6 +207,7 @@ end
 def useAidKit()
 	if $PokemonGlobal.teamHealerCurrentUses > 0
 		$PokemonGlobal.teamHealerCurrentUses -= 1
+		echoln("Aid kit info: #{AID_KIT_BASE_HEALING},#{HEALING_UPGRADE_AMOUNT},#{$PokemonGlobal.teamHealerUpgrades}")
 		healAmount = AID_KIT_BASE_HEALING + HEALING_UPGRADE_AMOUNT * $PokemonGlobal.teamHealerUpgrades
 		pbMessage(_INTL("Healing your entire team by {1}.",healAmount))
 		charges = $PokemonGlobal.teamHealerCurrentUses
