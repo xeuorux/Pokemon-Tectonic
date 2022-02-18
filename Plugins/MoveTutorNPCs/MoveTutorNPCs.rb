07e443edcb65ea899e4cdeb48b886d665315d8a8
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-02-17 17:06:44 -0700

    Pokemon can relearn moves they previously had mentored onto them

--- Plugins/MoveTutorNPCs/MoveTutorNPCs.rb
+++ Plugins/MoveTutorNPCs/MoveTutorNPCs.rb
@@ -241,7 +241,7 @@ def pbMentorMoveScreen(pkmn)
 	pbFadeOutIn {
 		scene = MoveLearner_Scene.new
 		screen = MoveLearnerScreen.new(scene)
-		retval = screen.pbStartScreen(pkmn,moves)
+		retval = screen.pbStartScreen(pkmn,moves,true)
 	}
 	return retval
 end
@@ -400,13 +400,14 @@ class MoveLearnerScreen
     @scene = scene
   end
 
-  def pbStartScreen(pkmn,moves)
+  def pbStartScreen(pkmn,moves,mentoring=false)
     @scene.pbStartScene(pkmn, moves)
     loop do
       move = @scene.pbChooseMove
       if move
         if @scene.pbConfirm(_INTL("Teach {1}?", GameData::Move.get(move).name))
           if pbLearnMove(pkmn, move)
+			pkmn.add_first_move(move) if mentoring
             @scene.pbEndScene
             return true
           end
