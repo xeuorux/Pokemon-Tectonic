b6e302e2daa291d9d261f3b61d7eb959182a1c5a
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-02-19 23:07:47 -0700

    Fix glitch where transforming into enemy pokemon made type icons appear on screen

--- Plugins/Battle/UI/PokemonDataBox.rb
+++ Plugins/Battle/UI/PokemonDataBox.rb
@@ -182,19 +182,19 @@ class PokemonDataBox < SpriteWrapper
 		end
 		if types[0]
 			@type1Icon.src_rect.y = GameData::Type.get(types[0]).id_number * TYPE_ICON_HEIGHT
-			@type1Icon.visible = true if @battler.effects[PBEffects::Transform]
+			@type1Icon.visible = true if @battler.effects[PBEffects::Transform] && @showTypes
 		else
 			@type1Icon.visible = false
 		end
 		if types[1]
 			@type2Icon.src_rect.y = GameData::Type.get(types[1]).id_number * TYPE_ICON_HEIGHT if types[1]
-			@type2Icon.visible = true if @battler.effects[PBEffects::Transform]
+			@type2Icon.visible = true if @battler.effects[PBEffects::Transform] && @showTypes
 		else
 			@type2Icon.visible = false
 		end
 		if types[2]
 			@type3Icon.src_rect.y = GameData::Type.get(types[2]).id_number * TYPE_ICON_HEIGHT if types[2]
-			@type3Icon.visible = true if @battler.effects[PBEffects::Transform]
+			@type3Icon.visible = true if @battler.effects[PBEffects::Transform] && @showTypes
 		else
 			@type3Icon.visible = false
 		end
