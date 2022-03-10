aa3399db8cb7ea39003a571345341098b71ac369
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-03-10 13:27:19 -0700

    Stat changes are show after swapping form with the universal formalizer
    Also fixed a crash with it
    Also refactored the EZPEZdispenser code a bit to avoid code duplication

--- Plugins/Items/New non battle items/EXPEZDispenser.rb
+++ Plugins/Items/New non battle items/EXPEZDispenser.rb
@@ -1,24 +1,5 @@
 ItemHandlers::UseFromBag.add(:EXPEZDISPENSER,proc { |item|
-	$PokemonGlobal.expJAR = 0 if $PokemonGlobal.expJAR.nil?
-	candyTotal = 0
-	pbMessage(_INTL("You have {1} EXP stored in the EXP-EZ Dispenser.",$PokemonGlobal.expJAR))
-	xsCandyTotal = $PokemonGlobal.expJAR / 350
-	sCandyTotal = xsCandyTotal / 4
-	xsCandyTotal = xsCandyTotal % 4
-	mCandyTotal = sCandyTotal / 4
-	sCandyTotal = sCandyTotal % 4
-	if sCandyTotal > 0 || xsCandyTotal > 0 || mCandyTotal > 0
-		if pbConfirmMessage(_INTL("You can make {1} Medium, {2} Small and {3} Extra-Small candies. Would you like to?", mCandyTotal, sCandyTotal, xsCandyTotal))
-			pbReceiveItem(:EXPCANDYM,mCandyTotal) if mCandyTotal > 0
-			pbReceiveItem(:EXPCANDYS,sCandyTotal) if sCandyTotal > 0
-			pbReceiveItem(:EXPCANDYXS,xsCandyTotal) if xsCandyTotal > 0
-			$PokemonGlobal.expJAR = $PokemonGlobal.expJAR % 350
-		end
-	else
-		pbMessage(_INTL("That's not enough to make any candies."))
-		next 0
-	end
-	next 1
+	return useEXPEZ
 })
 
 
@@ -27,6 +8,10 @@ ItemHandlers::ConfirmUseInField.add(:EXPEZDISPENSER,proc { |item|
 })
 
 ItemHandlers::UseInField.add(:EXPEZDISPENSER,proc { |item|
+	return useEXPEZ
+})
+
+def useEXPEZ()
 	$PokemonGlobal.expJAR = 0 if $PokemonGlobal.expJAR.nil?
 	candyTotal = 0
 	pbMessage(_INTL("You have {1} EXP stored in the EXP-EZ Dispenser.",$PokemonGlobal.expJAR))
@@ -47,4 +32,4 @@ ItemHandlers::UseInField.add(:EXPEZDISPENSER,proc { |item|
 		next 0
 	end
 	next 1
-})
+end
--- Plugins/Items/New non battle items/UniversalFormalizer.rb
+++ Plugins/Items/New non battle items/UniversalFormalizer.rb
@@ -14,9 +14,35 @@ ItemHandlers::UseOnPokemon.add(:UNIVERSALFORMALIZER,proc { |item,pkmn,scene|
 		end
 		possibleFormNames.push(_INTL("Cancel"))
 		choice = scene.pbMessage("Which form shall the Pokemon take?",	possibleFormNames,possibleFormNames.length)
-		if choice < possibleForm.length
+		if choice < possibleForms.length
+			# Mark down pre-evolution stats
+			oldTotalHP 	= pkmn.totalhp
+			oldAttack 	= pkmn.attack
+			oldDefense 	= pkmn.defense
+			oldSpAtk 	= pkmn.spatk
+			oldSpDef 	= pkmn.spdef
+			oldSpeed 	= pkmn.speed
+		
 			scene.pbMessage("#{pkmn.name} swapped to #{possibleFormNames[choice]}!")
 			pkmn.form = possibleForms[choice].form
+			
+			hpDif 		= pkmn.totalhp-oldTotalHP
+			attackDif 	= pkmn.attack-oldAttack
+			defenseDif 	= pkmn.defense-oldDefense
+			spAtkDif 	= pkmn.spatk-oldSpAtk
+			spDefDif 	= pkmn.spdef-oldSpDef
+			speedDif 	= pkmn.speed-oldSpeed
+			
+			if hpDif != 0 || attackDif != 0 || defenseDif != 0 ||
+				spAtkDif != 0 || spDefDif != 0 || speedDif != 0
+				pbTopRightWindow(
+					_INTL("Max. HP<r>#{hpDif >= 0 ? "+" : ""}{1}\r\nAttack<r>#{attackDif >= 0 ? "+" : ""}{2}\r\nDefense<r>#{defenseDif >= 0 ? "+" : ""}{3}\r\nSp. Atk<r>#{spAtkDif >= 0 ? "+" : ""}{4}\r\nSp. Def<r>#{spDefDif >= 0 ? "+" : ""}{5}\r\nSpeed<r>#{speedDif >= 0 ? "+" : ""}{6}",
+					hpDif,attackDif,defenseDif,
+					spAtkDif,spDefDif,speedDif))
+				pbTopRightWindow(
+					_INTL("Max. HP<r>{1}\r\nAttack<r>{2}\r\nDefense<r>{3}\r\nSp. Atk<r>{4}\r\nSp. Def<r>{5}\r\nSpeed<r>{6}",
+					pkmn.totalhp,pkmn.attack,pkmn.defense,pkmn.spatk,pkmn.spdef,pkmn.speed))
+			end
 		end
 		next true
 	else
