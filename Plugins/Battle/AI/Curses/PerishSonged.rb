91ebe2451d186095d446b8be8645ce41d058e402
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-03-13 23:55:40 -0700

    Extreme Perish song curse to just perish song curse

--- Plugins/Battle/AI/Curses/PerishSonged.rb
+++ Plugins/Battle/AI/Curses/PerishSonged.rb
@@ -1,7 +1,7 @@
 PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_PERISH_SONGED,
 	proc { |curse_policy,battle,curses_array|
-		battle.amuletActivates("Extreme Perish Song")
-		battle.pbDisplay(_INTL("Your Pokemon gain the \"Extreme Perish Song\" status when they enter battle.\1"))
+		battle.amuletActivates("Perish Song")
+		battle.pbDisplay(_INTL("Your Pokemon gain the \"Perish Song\" status when they enter battle.\1"))
 		curses_array.push(curse_policy)
 		next curses_array
 	}
@@ -10,8 +10,8 @@ PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_PERISH_SONGED,
 PokeBattle_Battle::BattlerEnterCurseEffect.add(:CURSE_PERISH_SONGED,
 	proc { |curse_policy,battler,battle|
 		next if battler.opposes?
-		battler.effects[PBEffects::PerishSong]     = 2
+		battler.effects[PBEffects::PerishSong]     = 3
     	battler.effects[PBEffects::PerishSongUser] = battler.index
-		battle.pbDisplay(_INTL("{1} heard the Extreme Perish Song! It will faint in two turns!",battler.pbThis))
+		battle.pbDisplay(_INTL("{1} heard the Perish Song! It will faint in three turns!",battler.pbThis))
 	}
 )
