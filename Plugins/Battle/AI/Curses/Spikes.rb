2b5e2811cc2b219f7aac01442e54b0c7ce2b6d3c
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-03-20 21:39:00 -0700

    Curse spike harder

--- Plugins/Battle/AI/Curses/Spikes.rb
+++ Plugins/Battle/AI/Curses/Spikes.rb
@@ -1,8 +1,8 @@
 PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_SPIKES,
 	proc { |curse_policy,battle,curses_array|
 		battle.amuletActivates("Spikes")
-		battle.sides[0].effects[PBEffects::Spikes] = 1
-		battle.pbDisplaySlower(_INTL("Spikes were scattered all around your Pokemon's feet!"))
+		battle.sides[0].effects[PBEffects::Spikes] = 3
+		battle.pbDisplaySlower(_INTL("Three layers of Spikes were scattered all around your Pokemon's feet!"))
 		curses_array.push(curse_policy)
 		next curses_array
 	}
