4ebb10dfdd84de33f0623c4b2cc981ea3fc49abe
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-04-03 11:46:07 -0700

    recoded the 'fix bad metadata values'

--- Plugins/Miscellaneous/Debug Commands/GlobalMetadata.rb
+++ Plugins/Miscellaneous/Debug Commands/GlobalMetadata.rb
@@ -34,14 +34,14 @@ DebugMenuCommands.register("initializemetadata", {
 DebugMenuCommands.register("reinitializebrokenmetadata", {
   "parent"      => "globalmetadata",
   "name"        => _INTL("Re-Initialize Broken Metadata"),
-  "description" => _INTL("Set nil global metadata values to new save file defaults."),
+  "description" => _INTL("Set undefined global metadata values to new save file defaults."),
   "effect"      => proc {
 	 newMetadat = PokemonGlobalMetadata.new
-	 $PokemonGlobal.instance_variables.each do |v|
+	 newMetadat.instance_variables.each do |v|
 		begin
 			varName = v.to_s.sub('@','')
 			defaultValue = newMetadat.send(varName)
-			if $PokemonGlobal.send(varName).nil? && !defaultValue.nil?
+			if !$PokemonGlobal.instance_variable_defined?(v)
 				$PokemonGlobal.send(varName + "=",defaultValue)
 				pbMessage(_INTL("Reset #{varName} to a value of #{defaultValue.to_s}."))
 			end
