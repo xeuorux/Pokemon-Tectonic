b48802637bede5c407ed96cd0b534b9b0b98ea0e
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-04-04 20:27:36 -0700

    Storing pokemon item take prompt name fix

--- Plugins/Miscellaneous/StoringPokemonChanges.rb
+++ Plugins/Miscellaneous/StoringPokemonChanges.rb
@@ -202,7 +202,7 @@ module PokeBattle_BattleCommon
 			
 			if chosenPokemon.hasItem?
 				itemName = GameData::Item.get(chosenPokemon.item).real_name
-				if pbConfirmMessageSerious(_INTL("{1} is holding an {2}. Would you like to take it before transferring?", pkmn.name, itemName))
+				if pbConfirmMessageSerious(_INTL("{1} is holding an {2}. Would you like to take it before transferring?", chosenPokemon.name, itemName))
 					pbTakeItemFromPokemon(chosenPokemon)
 				end
 			end
