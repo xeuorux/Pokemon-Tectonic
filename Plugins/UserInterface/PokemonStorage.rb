c99ba87ac39bea81d59368a0915ed31f3351a4ad
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-04-04 20:01:34 -0700

    Prompt to remove item from pokemon when depositing it to PC

--- Plugins/UserInterface/PokemonStorage.rb
+++ Plugins/UserInterface/PokemonStorage.rb
@@ -312,4 +312,157 @@ class PokemonStorageScreen
     end
     return
   end
+  
+  def pbStore(selected,heldpoke)
+    box = selected[0]
+    index = selected[1]
+    if box!=-1
+      raise _INTL("Can't deposit from box...")
+    end
+    if pbAbleCount<=1 && pbAble?(@storage[box,index]) && !heldpoke
+      pbPlayBuzzerSE
+      pbDisplay(_INTL("That's your last Pokémon!"))
+    elsif heldpoke && heldpoke.mail
+      pbDisplay(_INTL("Please remove the Mail."))
+    elsif !heldpoke && @storage[box,index].mail
+      pbDisplay(_INTL("Please remove the Mail."))
+    else
+      loop do
+        destbox = @scene.pbChooseBox(_INTL("Deposit in which Box?"))
+        if destbox>=0
+          firstfree = @storage.pbFirstFreePos(destbox)
+          if firstfree<0
+            pbDisplay(_INTL("The Box is full."))
+            next
+          end
+          if heldpoke || selected[0]==-1
+            p = (heldpoke) ? heldpoke : @storage[-1,index]
+            p.time_form_set = nil
+            p.form          = 0 if p.isSpecies?(:SHAYMIN)
+            p.heal
+          end
+          @scene.pbStore(selected,heldpoke,destbox,firstfree)
+          if heldpoke
+            @storage.pbMoveCaughtToBox(heldpoke,destbox)
+            @heldpkmn = nil
+          else
+            @storage.pbMove(destbox,-1,-1,index)
+          end
+        end
+        break
+      end
+      @scene.pbRefresh
+    end
+  end
+
+  def pbStore(selected,heldpoke)
+    box = selected[0]
+    index = selected[1]
+    if box!=-1
+      raise _INTL("Can't deposit from box...")
+    end
+    if pbAbleCount<=1 && pbAble?(@storage[box,index]) && !heldpoke
+      pbPlayBuzzerSE
+      pbDisplay(_INTL("That's your last Pokémon!"))
+    elsif heldpoke && heldpoke.mail
+      pbDisplay(_INTL("Please remove the Mail."))
+    elsif !heldpoke && @storage[box,index].mail
+      pbDisplay(_INTL("Please remove the Mail."))
+    else
+      loop do
+        destbox = @scene.pbChooseBox(_INTL("Deposit in which Box?"))
+        if destbox>=0
+          firstfree = @storage.pbFirstFreePos(destbox)
+          if firstfree<0
+            pbDisplay(_INTL("The Box is full."))
+            next
+          end
+          if heldpoke || selected[0]==-1
+            p = (heldpoke) ? heldpoke : @storage[-1,index]
+            p.time_form_set = nil
+            p.form          = 0 if p.isSpecies?(:SHAYMIN)
+            p.heal
+			tryTakeItem(p)
+          end
+		  
+          @scene.pbStore(selected,heldpoke,destbox,firstfree)
+          if heldpoke
+            @storage.pbMoveCaughtToBox(heldpoke,destbox)
+            @heldpkmn = nil
+          else
+            @storage.pbMove(destbox,-1,-1,index)
+          end
+        end
+        break
+      end
+      @scene.pbRefresh
+    end
+  end
+  
+  def pbPlace(selected)
+    box = selected[0]
+    index = selected[1]
+    if @storage[box,index]
+      raise _INTL("Position {1},{2} is not empty...",box,index)
+    end
+    if box!=-1 && index>=@storage.maxPokemon(box)
+      pbDisplay("Can't place that there.")
+      return
+    end
+    if box!=-1 && @heldpkmn.mail
+      pbDisplay("Please remove the mail.")
+      return
+    end
+    if box>=0
+      @heldpkmn.time_form_set = nil
+      @heldpkmn.form          = 0 if @heldpkmn.isSpecies?(:SHAYMIN)
+      @heldpkmn.heal
+	  tryTakeItem(@heldpkmn)
+    end
+    @scene.pbPlace(selected,@heldpkmn)
+    @storage[box,index] = @heldpkmn
+    if box==-1
+      @storage.party.compact!
+    end
+    @scene.pbRefresh
+    @heldpkmn = nil
+  end
+
+  def pbSwap(selected)
+    box = selected[0]
+    index = selected[1]
+    if !@storage[box,index]
+      raise _INTL("Position {1},{2} is empty...",box,index)
+    end
+    if box==-1 && pbAble?(@storage[box,index]) && pbAbleCount<=1 && !pbAble?(@heldpkmn)
+      pbPlayBuzzerSE
+      pbDisplay(_INTL("That's your last Pokémon!"))
+      return false
+    end
+    if box!=-1 && @heldpkmn.mail
+      pbDisplay("Please remove the mail.")
+      return false
+    end
+    if box>=0
+      @heldpkmn.time_form_set = nil
+      @heldpkmn.form          = 0 if @heldpkmn.isSpecies?(:SHAYMIN)
+      @heldpkmn.heal
+	  tryTakeItem(@heldpkmn)
+    end
+    @scene.pbSwap(selected,@heldpkmn)
+    tmp = @storage[box,index]
+    @storage[box,index] = @heldpkmn
+    @heldpkmn = tmp
+    @scene.pbRefresh
+    return true
+  end
+  
+  def tryTakeItem(pokemon)
+	if pokemon.hasItem?
+		itemName = GameData::Item.get(pokemon.item).real_name
+		if pbConfirmMessage(_INTL("Take {1}'s {2}?", pokemon.name, itemName))
+			pbTakeItemFromPokemon(pokemon)
+		end
+	end
+  end
 end
