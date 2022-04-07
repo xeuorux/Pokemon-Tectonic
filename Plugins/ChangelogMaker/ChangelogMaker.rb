d3e67505210b93ff3a2a98abe656f810f6335584
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-04-06 17:46:06 -0700

    Changelog maker outputs to txt file, much prettier formatting
    Also gives its own section in the debug menu

--- Plugins/ChangelogMaker/ChangelogMaker.rb
+++ Plugins/ChangelogMaker/ChangelogMaker.rb
@@ -1,18 +1,11 @@
-NEW_ABILITIES_STARTING_ID = 268
-
-MOVE_RENAMES = {
-			:CHARM => :POUT,
-			:ROCKSMASH => :SMASH,
-			:SMARTSTRIKE => :SMARTHORN,
-			:SWEETKISS => :ANGELSKISS,
-			:DREAMEATER => :DREAMABSORB,
-			:TOXICSPIKES => :POISONSPIKES
-		}
-
-CUT_MOVES = [:TOXIC,:DOUBLETEAM,:SANDATTACK,:IMPRISON,:FRUSTRATION,:RETURN,:HIDDENPOWER,:ATTRACT]
+DebugMenuCommands.register("changelog", {
+  "parent"      => "main",
+  "name"        => _INTL("Changelogs..."),
+  "description" => _INTL("Generate species changelogs of various types."),
+})
 
 DebugMenuCommands.register("generatechangelogrange", {
-  "parent"      => "editorsmenu",
+  "parent"      => "changelog",
   "name"        => _INTL("Generate species changelog between ids"),
   "description" => _INTL("See the changelog for each species between the Old and New pokemon.txt files for a specific range of IDs."),
   "effect"      => proc { |sprites, viewport|
@@ -33,7 +26,7 @@ DebugMenuCommands.register("generatechangelogrange", {
 })
 
 DebugMenuCommands.register("generatechangelog", {
-  "parent"      => "editorsmenu",
+  "parent"      => "changelog",
   "name"        => _INTL("Generate species changelog"),
   "description" => _INTL("See the changelog for each species between the Old and New pokemon.txt files."),
   "effect"      => proc { |sprites, viewport|
@@ -46,232 +39,217 @@ def printChangeLogBetween(firstNumberAttempt,lastNumberAttempt)
 	
 	generationLastNumbers = [0,151,251,386,493,649,721,809,898]
 	currentGeneration = 0
+
+	stats = [:HP,:ATTACK,:DEFENSE,:SPECIAL_ATTACK,:SPECIAL_DEFENSE,:SPEED]
+	statNames = {:HP => "HP",:ATTACK => "Attack",:DEFENSE => "Defense",:SPECIAL_ATTACK => "Sp. Atk",:SPECIAL_DEFENSE => "Sp. Def",:SPEED => "Speed"}
 	
-    GameData::SpeciesOld.each do |species_data|
-		next if species_data.form != 0
-		next if species_data.id_number < firstNumberAttempt
-		break if species_data.id_number > lastNumberAttempt
-		
-		if species_data.id_number > generationLastNumbers[currentGeneration]
-			currentGeneration += 1
+	File.open("changelog.txt","wb") { |f|
+
+		GameData::SpeciesOld.each do |species_data|
+			next if species_data.form != 0
+			next if species_data.id_number < firstNumberAttempt
+			break if species_data.id_number > lastNumberAttempt
 			
-			echoln("Generation #{currentGeneration}")
-		end
-		
-		newSpeciesData = GameData::Species.get(species_data.id) || nil
-		next if newSpeciesData.nil?
-		changeLog = []
-		
-		# Check for type changes
-		oldTypes = [species_data.type1]
-		oldTypes.push(species_data.type2) if species_data.type1 != species_data.type2
-		
-		newTypes = [newSpeciesData.type1]
-		newTypes.push(newSpeciesData.type2) if newSpeciesData.type1 != newSpeciesData.type2
-		
-		oldTypes.each do |oldType|
-			changeLog.push("Removed type #{oldType}") if !newTypes.include?(oldType)
-		end
-		
-		newTypes.each do |newType|
-			changeLog.push("Added type #{newType}") if !oldTypes.include?(newType)
-		end
-		
-		# Check for stat changes
-		stats = [:HP,:ATTACK,:DEFENSE,:SPECIAL_ATTACK,:SPECIAL_DEFENSE,:SPEED]
-		oldBST = 0
-		newBST = 0
-		stats.each do |s|
-				oldStat = species_data.base_stats[s]
-				oldBST += oldStat
-				newStat = newSpeciesData.base_stats[s]
-				newBST += newStat
-				if oldStat != newStat
-					difference = (newStat - oldStat)
-					difference = "+" + difference.to_s if difference > 0
-					changeLog.push("#{s}: #{oldStat} => #{newStat} (#{difference})")
-				end
-		end
-		bstDiff = (newBST - oldBST)
-		bstDiff = "+" + bstDiff.to_s if bstDiff > 0
-		changeLog.push("BST: #{oldBST} => #{newBST} (#{bstDiff})") if oldBST != newBST
-		
-		# Check for ability changes
-		abilityRenames = {
-			:ADAPTABILITY => :ADAPTED,
-			:QUEENLYMAJESTY => :ROYALMAJESTY
-		}
-		if species_data.abilities[0] != newSpeciesData.abilities[0]
-			renamed = abilityRenames[species_data.abilities[0]] || nil
-			if renamed != newSpeciesData.abilities[0]
-				if species_data.abilities[0].nil?
-					changeLog.push("Ability 1: Added #{newSpeciesData.abilities[0]}")
-				else
-					if newSpeciesData.abilities[0].nil?
-						changeLog.push("Ability 1: #{species_data.abilities[0]} removed")
-					else
-						changeLog.push("Ability 1: #{species_data.abilities[0]} => #{newSpeciesData.abilities[0]}")
+			if species_data.id_number > generationLastNumbers[currentGeneration]
+				currentGeneration += 1
+				
+				f.write("GENERATION #{currentGeneration}\r\n")
+			end
+			
+			newSpeciesData = GameData::Species.get(species_data.id) || nil
+			next if newSpeciesData.nil?
+			changeLog = []
+			
+			# Check for type changes
+			oldTypes = [species_data.type1]
+			oldTypes.push(species_data.type2) if species_data.type1 != species_data.type2
+			
+			newTypes = [newSpeciesData.type1]
+			newTypes.push(newSpeciesData.type2) if newSpeciesData.type1 != newSpeciesData.type2
+			
+			oldTypes.each do |oldType|
+				changeLog.push("Removed type #{GameData::Type.get(oldType).real_name}") if !newTypes.include?(oldType)
+			end
+			
+			newTypes.each do |newType|
+				changeLog.push("Added type #{GameData::Type.get(newType).real_name}") if !oldTypes.include?(newType)
+			end
+			
+			# Check for stat changes
+			
+			oldBST = 0
+			newBST = 0
+			stats.each do |s|
+					oldStat = species_data.base_stats[s]
+					oldBST += oldStat
+					newStat = newSpeciesData.base_stats[s]
+					newBST += newStat
+					if oldStat != newStat
+						difference = (newStat - oldStat)
+						difference = "+" + difference.to_s if difference > 0
+						spacesString = ""
+						(12-statNames[s].length).times do
+							spacesString += " "
+						end
+						changeLog.push("#{statNames[s]}: #{spacesString}#{oldStat} => #{newStat} (#{difference})")
+					end
+			end
+			bstDiff = (newBST - oldBST)
+			bstDiff = "+" + bstDiff.to_s if bstDiff > 0
+			changeLog.push("Stat total:   #{oldBST} => #{newBST} (#{bstDiff})") if oldBST != newBST
+			
+			# Check for ability changes
+			[0,1].each do |i|
+				if species_data.abilities[i] != newSpeciesData.abilities[i]
+					renamed = ABILITY_RENAMES[species_data.abilities[i]] || nil
+					if renamed != newSpeciesData.abilities[i]
+						if !newSpeciesData.abilities[i].nil?
+							newAbilityData = GameData::Ability.get(newSpeciesData.abilities[i])
+							newRealName = newAbilityData.real_name
+						end
+						if species_data.abilities[i].nil?
+							changeLog.push("Ability #{i+1}:    Added #{newRealName}")
+						else
+							oldAbilityData = GameData::Ability.get(species_data.abilities[i])
+							oldRealName = oldAbilityData.real_name
+							if newSpeciesData.abilities[i].nil?
+								changeLog.push("Ability #{i+1}:    #{oldRealName} removed")
+							else
+								changeLog.push("Ability #{i+1}:    #{oldRealName} => #{newRealName}")
+							end
+						end
+						# Write out the description of the new ability if its custom
+						if !newSpeciesData.abilities[i].nil? && newAbilityData.id_number > NEW_ABILITIES_STARTING_ID
+							desc = newAbilityData.description.gsub("%"," percent")
+							changeLog.push("\t\"#{desc}\"")
+						end
 					end
 				end
-				if !newSpeciesData.abilities[0].nil?
-					abilityData = GameData::Ability.get(newSpeciesData.abilities[0])
-					desc = abilityData.description.gsub("%"," percent")
-					changeLog.push("* #{abilityData.real_name} is a custom ability: \"#{desc}\"") if abilityData.id_number > NEW_ABILITIES_STARTING_ID
+			end
+					
+			#Check for which specific moves have been changed
+			moveRenamesInverted = MOVE_RENAMES.invert
+			
+			oldMovesLearned = []
+			species_data.moves.each do |learnsetEntry|
+				oldMovesLearned.push(learnsetEntry[1])
+			end
+			species_data.tutor_moves.each do |move|
+				oldMovesLearned.push(move)
+			end
+			species_data.egg_moves.each do |move|
+				oldMovesLearned.push(move)
+			end
+			oldMovesLearned.uniq!
+			oldMovesLearned.compact!
+			
+			newMovesLearned = []
+			newSpeciesData.moves.each do |learnsetEntry|
+				newMovesLearned.push(learnsetEntry[1])
+			end
+			newSpeciesData.tutor_moves.each do |move|
+				newMovesLearned.push(move)
+			end
+			newSpeciesData.egg_moves.each do |move|
+				newMovesLearned.push(move)
+			end
+			newMovesLearned.uniq!
+			newMovesLearned.compact!
+			
+			cutMoves = []
+			
+			oldMovesLearned.each do |oldMove|
+				moveRename = MOVE_RENAMES[oldMove] || oldMove
+				if !newMovesLearned.include?(moveRename) && !CUT_FROM_EVERYONE.include?(moveRename)
+					cutMoves.push(oldMove)
 				end
 			end
-		end
-		if species_data.abilities[1] != newSpeciesData.abilities[1]
-			renamed = abilityRenames[species_data.abilities[1]] || nil
-			if renamed != newSpeciesData.abilities[1]
-				if species_data.abilities[1].nil?
-					changeLog.push("Ability 2: Added #{newSpeciesData.abilities[1]}")
-				else
-					if newSpeciesData.abilities[1].nil?
-						changeLog.push("Ability 1: #{species_data.abilities[1]} removed")
-					else
-						changeLog.push("Ability 2: #{species_data.abilities[1]} => #{newSpeciesData.abilities[1]}")
+			
+			if cutMoves.length > 0
+				str = "Removed Moves: \r\n\t"
+				cutMoves.each_with_index do |move,index|
+					str += GameData::Move.get(move).real_name
+					str += ", " if index != cutMoves.length - 1
+				end
+				changeLog.push(str)
+			end
+			
+			newMoves = []
+			
+			newMovesLearned.each do |newMove|
+				moveRename = moveRenamesInverted[newMove] || newMove
+				newMoves.push(newMove) if !oldMovesLearned.include?(moveRename) && !ADDED_TO_EVERYONE.include?(moveRename)
+			end
+			
+			if newMoves.length > 0
+				str = "Added Moves: \r\n\t"
+				newMoves.each_with_index do |move,index|
+					str += GameData::Move.get(move).real_name
+					str += ", " if index != newMoves.length - 1
+				end
+				changeLog.push(str)
+			end
+			
+			# Check for evolution changes
+			species_data.evolutions.each do |evolutionData|
+				next if evolutionData[3]
+				method = evolutionData[1]
+				parameter = evolutionData[2]
+				species = evolutionData[0]
+				speciesName = GameData::Species.get(species).real_name
+				
+				speciesStillThere = false
+				newSpeciesData.evolutions.each do |newEvolutionData|
+					next if newEvolutionData[3]
+					newSpecies = newEvolutionData[0]
+					newMethod = newEvolutionData[1]
+					newParameter = newEvolutionData[2]
+					if species == newSpecies
+						speciesStillThere = true
+						if method == newMethod
+							if parameter != newParameter
+								changeLog.push("Evolution to #{speciesName} parameter: #{parameter} => #{newParameter}") 
+							end
+						else
+							descriptor = method.to_s
+							descriptor += "(" + parameter.to_s + ")" if !parameter.nil?
+							newDescriptor = newMethod.to_s
+							newDescriptor += "(" + newParameter.to_s + ")" if !newParameter.nil?
+							changeLog.push("Evolution to #{speciesName} method: #{descriptor} => #{newDescriptor}") 
+						end
+						break
 					end
 				end
-				if !newSpeciesData.abilities[1].nil?
-					abilityData = GameData::Ability.get(newSpeciesData.abilities[1])
-					desc = abilityData.description.gsub("%"," percent")
-					changeLog.push("* #{abilityData.real_name} is a custom ability: \"#{desc}\"") if abilityData.id_number > NEW_ABILITIES_STARTING_ID
-				end
+				changeLog.push("Evolution to #{speciesName} cut.") if !speciesStillThere
 			end
-		end
-		
-=begin
-		# Check for move list changes
-		if species_data.moves != newSpeciesData.moves
-			changeLog.push("Learnset changed.")
-		end
-		if species_data.tutor_moves != newSpeciesData.tutor_moves
-			changeLog.push("Tutor learnset changed.")
-		end
-		if species_data.egg_moves != newSpeciesData.egg_moves
-			changeLog.push("Egg moveset changed.")
-		end
-=end
-		
-		#Check for which specific moves have been changed
-		moveRenamesInverted = MOVE_RENAMES.invert
-		
-		oldMovesLearned = []
-		species_data.moves.each do |learnsetEntry|
-			oldMovesLearned.push(learnsetEntry[1])
-		end
-		species_data.tutor_moves.each do |move|
-			oldMovesLearned.push(move)
-		end
-		species_data.egg_moves.each do |move|
-			oldMovesLearned.push(move)
-		end
-		oldMovesLearned.uniq!
-		oldMovesLearned.compact!
-		
-		newMovesLearned = []
-		newSpeciesData.moves.each do |learnsetEntry|
-			newMovesLearned.push(learnsetEntry[1])
-		end
-		newSpeciesData.tutor_moves.each do |move|
-			newMovesLearned.push(move)
-		end
-		newSpeciesData.egg_moves.each do |move|
-			newMovesLearned.push(move)
-		end
-		newMovesLearned.uniq!
-		newMovesLearned.compact!
-		
-		cutMoves = []
-		
-		oldMovesLearned.each do |oldMove|
-			moveRename = MOVE_RENAMES[oldMove] || oldMove
-			if !newMovesLearned.include?(moveRename) && !CUT_MOVES.include?(moveRename)
-				cutMoves.push(oldMove)
-			end
-		end
-		
-		if cutMoves.length > 0
-			str = "Removed Moves: "
-			cutMoves.each_with_index do |move,index|
-				str += move.to_s
-				str += ", " if index != cutMoves.length - 1
-			end
-			changeLog.push(str)
-		end
-		
-		newMoves = []
-		
-		newMovesLearned.each do |newMove|
-			moveRename = moveRenamesInverted[newMove] || newMove
-			newMoves.push(newMove) if !oldMovesLearned.include?(moveRename)
-		end
-		
-		if newMoves.length > 0
-			str = "Added Moves: "
-			newMoves.each_with_index do |move,index|
-				str += move.to_s
-				str += ", " if index != newMoves.length - 1
-			end
-			changeLog.push(str)
-		end
-		
-		# Check for evolution changes
-		species_data.evolutions.each do |evolutionData|
-			next if evolutionData[3]
-			method = evolutionData[1]
-			parameter = evolutionData[2]
-			species = evolutionData[0]
 			
-			speciesStillThere = false
 			newSpeciesData.evolutions.each do |newEvolutionData|
-				next if newEvolutionData[3]
 				newSpecies = newEvolutionData[0]
+				newSpeciesName = GameData::Species.get(newSpecies).real_name
 				newMethod = newEvolutionData[1]
 				newParameter = newEvolutionData[2]
-				if species == newSpecies
-					speciesStillThere = true
-					if method == newMethod
-						if parameter != newParameter
-							changeLog.push("Evolution to #{species} parameter: #{parameter} => #{newParameter}") 
-						end
-					else
-						descriptor = method.to_s
-						descriptor += "(" + parameter.to_s + ")" if !parameter.nil?
-						newDescriptor = newMethod.to_s
-						newDescriptor += "(" + newParameter.to_s + ")" if !newParameter.nil?
-						changeLog.push("Evolution to #{species} method: #{descriptor} => #{newDescriptor}") 
-					end
-					break
+				didEvoExistBefore = false
+				species_data.evolutions.each do |evolutionData|
+					didEvoExistBefore = true if newSpecies == evolutionData[0]
+				end
+				if !didEvoExistBefore
+					changeLog.push("Evolution added: #{newSpeciesName}: #{newMethod},#{newParameter}")
 				end
 			end
-			changeLog.push("Evolution to #{species} cut.") if !speciesStillThere
-		end
-		
-		newSpeciesData.evolutions.each do |newEvolutionData|
-			newSpecies = newEvolutionData[0]
-			newMethod = newEvolutionData[1]
-			newParameter = newEvolutionData[2]
-			didEvoExistBefore = false
-			species_data.evolutions.each do |evolutionData|
-				didEvoExistBefore = true if newSpecies == evolutionData[0]
-			end
-			if !didEvoExistBefore
-				changeLog.push("Evolution added: #{newSpecies},#{newMethod},#{newParameter}")
+			
+			# Print out the changelog
+			if changeLog.length == 0
+				unchanged.push(species_data.id)
+			else
+				f.write("#{species_data.real_name} changelog:\r\n")
+				changeLog.each do |change|
+					next if change.nil?
+					f.write(change + "\r\n")
+				end
+				f.write("\r\n")
 			end
 		end
-		
-		# Print out the changelog
-		if changeLog.length == 0
-			unchanged.push(species_data.id)
-		else
-			echoln("#{species_data.real_name} changelog:")
-			changeLog.each do |change|
-				next if change.nil?
-				echoln(change)
-			end
-			echoln("")
-		end
-	end
-	echoln("Species that were unchanged: #{unchanged.to_s}")
+		f.write("Species that were unchanged: #{unchanged.to_s}")
+	}
+	pbMessage(_INTL("Species changelog written to changelog.txt"))
 end
