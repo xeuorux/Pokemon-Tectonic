3c5c3a703b238a1a5ad5ccaacc86536bf16e2647
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-04-07 12:35:48 -0700

    mimic pokeballs look slightly different

--- Plugins/Battle/Moves/ModifiedMoveCodes.rb
+++ Plugins/Battle/Moves/ModifiedMoveCodes.rb
@@ -1201,13 +1201,13 @@ class PokeBattle_Move_096 < PokeBattle_Move
   def pbBaseType(user)
     item = user.item
     ret = :NORMAL
-	if !item.nil?
-		@typeArray.each do |type, items|
-		  next if !items.include?(item.id)
-		  ret = type if GameData::Type.exists?(type)
-		  break
-		end
-	end
+    if !item.nil?
+      @typeArray.each do |type, items|
+        next if !items.include?(item.id)
+        ret = type if GameData::Type.exists?(type)
+        break
+      end
+    end
     return ret
   end
 end
