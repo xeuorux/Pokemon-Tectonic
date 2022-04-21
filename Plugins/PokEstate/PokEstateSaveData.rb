83b08e8be241d5c4ec3b5ab3994afcdb86c43aa6
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-04-20 17:51:58 -0700

    Update PokEstateSaveData.rb

--- Plugins/PokEstate/PokEstateSaveData.rb
+++ Plugins/PokEstate/PokEstateSaveData.rb
@@ -9,6 +9,6 @@ SaveData.register_conversion(:pokestate_2) do
   game_version '1.6.1'
   display_title 'Adding PokEstate object to pre 1.6.1 saves.'
   to_all do |save_data|
-    save_data[:pokestate_2] = PokEstate.new if !save_data.has_key?(:pokestate)
+    save_data[:pokestate_2] = PokEstate.new if !save_data.has_key?(:pokestate_2)
   end
 end
