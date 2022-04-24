d6c05a3fdc9f58a39f9042b2dbbda9f9be895c08
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-04-24 00:24:08 -0700

    Eevee raffle works now

--- Plugins/Miscellaneous/Event Helpers/Misc.rb
+++ Plugins/Miscellaneous/Event Helpers/Misc.rb
@@ -61,25 +61,32 @@ def playerIsOutdoors?
 	end
 end
 
-def pokemonRaffle(species,level=10,cost=200,baseChance=3.0,chanceIncrease=1.5,disablingSwitch='B')
+class PokemonGlobalMetadata
+	attr_accessor :raffleChancesTried
+end
+
+def pokemonRaffle(species,level=10,cost=200,baseChance=5.0,chanceIncrease=1.5,disablingSwitch='A')
 	$PokemonGlobal.raffleChancesTried = {} if $PokemonGlobal.raffleChancesTried.nil?
-	$PokemonGlobal.raffleChancesTried[species] = 0 if !$PokemonGlobal.raffleChancesTried.include_key?(species)
+	$PokemonGlobal.raffleChancesTried[species] = 0 if !$PokemonGlobal.raffleChancesTried.has_key?(species)
 	speciesName = GameData::Species.get(species).real_name
-	if pbMessageConfirmSerious(_INTL("We're running a raffle. Would you like to sepnd $#{cost} on a chance to win a #{speciesName}?"))
+	if pbConfirmMessageSerious(_INTL("We're running a raffle. Would you like to sepnd $#{cost} on a chance to win a #{speciesName}?"))
 		if $Trainer.money < cost
 			pbMessage(_INTL("I'm sorry, but you don't seem to have enough money."))
 		else
 			$Trainer.money -= cost
 			pbMessage(_INTL("You hand over $#{cost}."))
 			chance = baseChance + chanceIncrease * $PokemonGlobal.raffleChancesTried[species]
-			pbMessage(_INTL("Alright, let me roll for you\\|.\\|.\\|."))
-			if pbRandom(100 < chance)
+			roll = rand(100)
+			echoln("Raffle chance and roll: #{chance},#{roll}")
+			pbMessage(_INTL("Alright, let me roll for you...\\|"))
+			if roll < chance
 				pbMessage(_INTL("Congratulations, you have won the raffle! Here is your #{speciesName}, as promised."))
 				pbAddPokemon(species,level)
 				setMySwitch(disablingSwitch,true)
 			else
 				pbMessage(_INTL("No luck! Try again next time."))
 			end
+			$PokemonGlobal.raffleChancesTried[species] += 1
 		end
 	end
 end
