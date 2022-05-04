0baa7e0a1390b4e49f3037b0bee86e78ed80ba75
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-05-03 19:56:21 -0700

    Encounter tile particles only every 4 steps, other small fixes

--- Plugins/Miscellaneous/EncounterParticleEffects.rb
+++ Plugins/Miscellaneous/EncounterParticleEffects.rb
@@ -4,19 +4,24 @@ Events.onStepTakenFieldMovement += proc { |_sender, e|
   event = e[0]   # Get the event affected by field movement
   if $scene.is_a?(Scene_Map)
     if $PokemonSystem.particle_effects == 0 && !event.floats
-		event.each_occupied_tile do |x, y|
-		  tag = $MapFactory.getTerrainTag(event.map.map_id, x, y, true)
-		  if tag.shows_grass_rustle
-			$scene.spriteset.addUserAnimation(Settings::GRASS_ANIMATION_ID, x, y, true, 1)
-		  elsif tag == :Puddle
-			$scene.spriteset.addUserAnimation(8, x, y, true, 1)
-		  elsif tag == :FishingContest
-			$scene.spriteset.addUserAnimation(8, x, y, true, 1)
-		  elsif tag == :SewerFloor || tag == :SewerWater
-		   $scene.spriteset.addUserAnimation(18, x, y, true, 1)
-		  elsif tag == :DarkCave
-			$scene.spriteset.addUserAnimation(2, x, y, true, 1)
-		  end
+		$globalParticleCounter = 0 if $globalParticleCounter.nil?
+		$globalParticleCounter += 1
+		if $globalParticleCounter >= 4
+			$globalParticleCounter = 0
+			event.each_occupied_tile do |x, y|
+				tag = $MapFactory.getTerrainTag(event.map.map_id, x, y, true)
+				if tag.shows_grass_rustle
+					$scene.spriteset.addUserAnimation(Settings::GRASS_ANIMATION_ID, x, y, true, 1)
+				elsif tag == :Puddle
+					$scene.spriteset.addUserAnimation(8, x, y, true, 1)
+				elsif tag == :FishingContest
+					$scene.spriteset.addUserAnimation(8, x, y, true, 1)
+				elsif tag == :SewerFloor
+				$scene.spriteset.addUserAnimation(18, x, y, true, 1)
+				elsif tag == :DarkCave
+					$scene.spriteset.addUserAnimation(2, x, y, true, 1)
+				end
+			end
 		end
 	end
 	# Slide on ice
