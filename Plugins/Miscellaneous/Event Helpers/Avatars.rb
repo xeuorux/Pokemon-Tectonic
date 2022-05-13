bf75b0aae66c8f284b4e1be998fc939f53618ec9
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-05-13 16:33:12 -0700

    defeatMultipleBosses helper method

--- Plugins/Miscellaneous/Event Helpers/Avatars.rb
+++ Plugins/Miscellaneous/Event Helpers/Avatars.rb
@@ -5,7 +5,6 @@ end
 def defeatBoss(item=nil,count=1)
 	$PokemonGlobal.respawnPoint = nil
 
-	speed = 20
 	event = get_self
 	255.downto(0) do |i|
 		next if i % 3 != 0
@@ -20,6 +19,32 @@ def defeatBoss(item=nil,count=1)
 		pbReceiveItem(item)
 	elsif count > 1
 		pbMessage("It left behind some items!")
+		pbReceiveItem(item,count)
+	end
+end
+
+def defeatMultipleBosses(item=nil,count=1,eventIDs=[])
+	$PokemonGlobal.respawnPoint = nil
+
+	events = eventIDs.map! { |eventID| get_character(eventID)}
+	255.downto(0) do |i|
+		next if i % 3 != 0
+		events.each do |event|
+			event.opacity = i
+		end
+		pbWait(1)
+	end
+
+	eventIDs.each do |id|
+		pbSetSelfSwitch(id,'A',true,$game_map.map_id)
+	end
+
+	return if item == nil
+	if count == 1
+		pbMessage("They left behind an item!")
+		pbReceiveItem(item)
+	elsif count > 1
+		pbMessage("They left behind some items!")
 		pbReceiveItem(item,count)
 	end
 end
