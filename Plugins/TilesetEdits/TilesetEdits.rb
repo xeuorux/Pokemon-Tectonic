a81b06b2878060251e7ec14ba2d6d09317cc0a78
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-05-20 10:55:04 -0700

    TilesetEdits: Can blank out single tile from tileset

--- Plugins/TilesetEdits/TilesetEdits.rb
+++ Plugins/TilesetEdits/TilesetEdits.rb
@@ -33,17 +33,7 @@ class PokemonTilesetScene
             end
           elsif Input.trigger?(Input::BACK)
             if pbConfirmMessage(_INTL("Save changes?"))
-              save_data(@tilesets_data, "Data/Tilesets.rxdata")
-              $data_tilesets = @tilesets_data
-              if $game_map && $MapFactory
-                $MapFactory.setup($game_map.map_id)
-                $game_player.center($game_player.x, $game_player.y)
-                if $scene.is_a?(Scene_Map)
-                  $scene.disposeSpritesets
-                  $scene.createSpritesets
-                end
-              end
-              pbMessage(_INTL("To ensure that the changes remain, close and reopen RPG Maker XP."))
+              saveTileSetChanges()
             end
             break if pbConfirmMessage(_INTL("Exit from the editor?"))
           elsif Input.trigger?(Input::USE)
@@ -54,12 +44,13 @@ class PokemonTilesetScene
             set_terrain_tag_for_tile_ID(selected, pbMessageChooseNumber(_INTL("Set the terrain tag."), params))
             draw_overlay
           elsif Input.trigger?(Input::SPECIAL)
+            # Passges goes in this order
+            # Counter Flag, Bush Flag, blank, blank, [directions here in some order]
             cmdRemoveUses = -1
             cmdEraseTile = -1
             cmdSwapTile = -1
             cmdInsertLine = -1
             cmdDeleteLine = -1
-            
 
             tileEditCommands = [_INTL("Cancel")]
             tileEditCommands[cmdRemoveUses = tileEditCommands.length] = _INTL("Remove Tile Uses")
@@ -80,15 +71,15 @@ class PokemonTilesetScene
               @tileset.terrain_tags[selected] = 0
               @tileset.priorities[selected] = 0
               @tileset.passages[selected] = 0x00
-              mapData.saveTilesets
+              @tileset.bush_flags[selected] = 0
 
               tileDrawer = TileDrawingHelper.fromTileset(@tileset)
               tileSetFileName = "Graphics/Tilesets/" + @tileset.tileset_name
-              tilesetBitmap = AnimatedBitmap.new(tileSetFileName)
-              bitmapTopLeftX = @x * TILE_SIZE
-              bitmapTopLeftY = @y * TILE_SIZE
+              tilesetBitmap = AnimatedBitmap.new(tileSetFileName).bitmap
+              bitmapTopLeftX = (@x) * TILE_SIZE
+              bitmapTopLeftY = (@y - 1) * TILE_SIZE
               bitmapBottomRightX = (@x + 1) * TILE_SIZE
-              bitmapBottomRightY = (@y + 1) * TILE_SIZE
+              bitmapBottomRightY = (@y) * TILE_SIZE
               blankColor = Color.new(0,0,0,0)
               for x in bitmapTopLeftX..bitmapBottomRightX
                 for y in bitmapTopLeftY..bitmapBottomRightY
@@ -96,6 +87,15 @@ class PokemonTilesetScene
                 end
               end
               tilesetBitmap.to_file(tileSetFileName + '.png')
+
+              if pbConfirmMessageSerious(_INTL("Delete all references to this tile?"))
+                editTilesOnAllMaps(@tileset.id,[[selected,0]])
+              end
+
+              saveTileSetChanges()
+
+              draw_tiles
+              draw_overlay
             elsif cmdInsertLine > -1 && tileCommand == cmdInsertLine
               # TODO
               # .insert()
@@ -142,5 +142,19 @@ class PokemonTilesetScene
               mapData.saveMap(id)
             end
         end
+    end
+
+    def saveTileSetChanges
+      save_data(@tilesets_data, "Data/Tilesets.rxdata")
+      $data_tilesets = @tilesets_data
+      if $game_map && $MapFactory
+        $MapFactory.setup($game_map.map_id)
+        $game_player.center($game_player.x, $game_player.y)
+        if $scene.is_a?(Scene_Map)
+          $scene.disposeSpritesets
+          $scene.createSpritesets
+        end
+      end
+      pbMessage(_INTL("To ensure that the changes remain, close and reopen RPG Maker XP."))
     end
 end
