95d16a8f5bb76410662e5dce68798f58b6b1b85f
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-06-07 09:06:08 -0700

    some glossary work

--- Plugins/UserInterface/GlossaryEntryLister.rb
+++ Plugins/UserInterface/GlossaryEntryLister.rb
@@ -1,9 +1,24 @@
 MAIN_GLOSSARY_HASH = {
-    "Moves" => "What are moves and how do they work?"
+    "Basic Battle Mechanics" => "How do battles work?",
+    "Moves" => "What are moves and what are the differences between them?",
+    "Stats" => "What are stats, and how do they effect battles?",
+    "Abilities" => "How do abilities work?",
+    "Held Items" => "What are held items and how to use them?",
+    "Trainers" => "How do enemy trainers work?",
+    "Avatars" => "What are avatars and what do they do?",
+}
+
+BASICS_GLOSSARY_HASH
+{
+    "Taking Turns" = "",
 }
 
 MOVE_GLOSSARY_HASH = {
-    "Physical vs Special" => "asjdbasjod"
+    "Attacking vs Status" => "Attacking moves deal damage. Status moves do not. Status moves are notated by a Yin/Yang symbol.",
+    "Physical vs Special" => "Attacking moves are split into Physical moves, and Special moves. Each of these categories uses different stats to determine how much damage is dealt to the target.",
+    "Physical Moves" => "Physical moves are notated by the symbol of a smashing fist. Theyir damage is based on the Attack (Atk) stat of the attacker and the Defense (Def) stat of the target.",
+    "Special Moves" => "Special moves are notated by the symbol of a splash in water. Their is based on the Special Attack (Sp. Atk) of the attacker and the Special Defense (Sp. Def) of the target.",
+    "Targeting" => "Most moves target only a single Pokemon, but many can target multiple Pokemon at once. Some moves differ in how far they can target in the bigger battle styles (doubles, triples).",
 }
 
 class GlossaryEntryList
