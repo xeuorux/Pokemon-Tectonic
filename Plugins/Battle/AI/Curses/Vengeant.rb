2c124e31fa9255c7270e0f47a4bb7bd35c43c9a1
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-06-17 00:14:10 -0700

    Vengeant curse based on fainters max HP instead of last damage hit

--- Plugins/Battle/AI/Curses/Vengeant.rb
+++ Plugins/Battle/AI/Curses/Vengeant.rb
@@ -1,7 +1,7 @@
 PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_VENGEANT,
 	proc { |curse_policy,battle,curses_array|
 		battle.amuletActivates("Vengeant")
-		battle.pbDisplaySlower(_INTL("When enemy Pokemon faint, they deal back to their last attacker the last amount of HP they lost."))
+		battle.pbDisplaySlower(_INTL("When enemy Pokemon faint, they deal back to their last attacker damage equal to one quarter of their max HP."))
 		curses_array.push(curse_policy)
 		next curses_array
 	}
@@ -11,13 +11,13 @@ PokeBattle_Battle::BattlerFaintedCurseEffect.add(:CURSE_VENGEANT,
 	proc { |curse_policy,battler,battle|
 		next if !battler.opposes?
 		next if battler.lastFoeAttacker.length == 0
-		next if battler.lastHPLost.nil?
 		battle.pbDisplay(_INTL("#{battler.pbThis} takes its vengeance!"))
+		hpLoss = battler.totalhp / 4
 		battler.lastFoeAttacker.each do |battler_index|
 			affected = battle.battlers[battler_index]
 			if affected.takesIndirectDamage?(PokeBattle_SceneConstants::USE_ABILITY_SPLASH)
 				battle.scene.pbDamageAnimation(affected)
-				affected.pbReduceHP(battler.lastHPLost,false)
+				affected.pbReduceHP(hpLoss,false)
 			end
 		end
 	}
