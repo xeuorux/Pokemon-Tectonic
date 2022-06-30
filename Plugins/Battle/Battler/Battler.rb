8456f007063d1e2d418459c472ba2c96d776f9e7
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-06-30 03:04:26 -0700

    Burrower also gives sandstorm immunity

--- Plugins/Battle/Battler/Battler.rb
+++ Plugins/Battle/Battler/Battler.rb
@@ -62,7 +62,7 @@ class PokeBattle_Battler
 		return false if !takesIndirectDamage?
 		return false if pbHasType?(:GROUND) || pbHasType?(:ROCK) || pbHasType?(:STEEL)
 		return false if inTwoTurnAttack?("0CA","0CB")   # Dig, Dive
-		return false if hasActiveAbility?([:OVERCOAT,:SANDFORCE,:SANDRUSH,:SANDSHROUD,:STOUT,:DESERTSPIRIT])
+		return false if hasActiveAbility?([:OVERCOAT,:SANDFORCE,:SANDRUSH,:SANDSHROUD,:STOUT,:DESERTSPIRIT,:BURROWER])
 		return false if hasActiveItem?(:SAFETYGOGGLES)
 		return true
 	  end
