2c0a6a9299d6bd5fc4fa11c89c97b841055e57e2
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-07-03 03:19:09 -0700

    Debug method and save converter to reassign item pockets

---
+++ Plugins/Items/BagHelpers.rb
@@ -0,0 +1,22 @@
+class PokemonBag
+    def reassignPockets()
+        hashOfAllItems = {}
+        for i in 0..PokemonBag.numPockets
+            @pockets[i].each do |item_element|
+                itemID = item_element[0]
+                itemCount = item_element[1]
+                if hashOfAllItems.has_key?(itemID)
+                    hashOfAllItems += itemCount
+                else
+                    hashOfAllItems[itemID] = itemCount
+                end
+            end
+            @pockets[i] = []
+        end
+
+        hashOfAllItems.each do |key, value|
+            echoln("Storing #{value} of #{key} back into the bag")
+            pbStoreItem(key,value)
+        end
+    end
+end
