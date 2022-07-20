240f28261d3ef04abbddc837666ae66c1b011105
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-07-20 16:06:00 -0700

    Totem selection UI shows waypoint name in top right

--- Plugins/UserInterface/TownMap.rb
+++ Plugins/UserInterface/TownMap.rb
@@ -142,6 +142,7 @@ class PokemonRegionMap_Scene
         @sprites["cursor"].y = newY-yOffset
         next
       end
+      @sprites["mapbottom"].waypointName = $waypoints_tracker.getTotemAtMapPosition(@mapX,@mapY) || ""
       @sprites["mapbottom"].maplocation = pbGetMapLocation(@mapX,@mapY)
       @sprites["mapbottom"].mapdetails  = pbGetMapDetails(@mapX,@mapY)
       ox = 0
@@ -218,4 +219,42 @@ def getDisplayedPositionOfGameMap(gameMapID)
     end
   end
   return displayedPosition
+end
+
+class MapBottomSprite < SpriteWrapper
+  attr_reader :mapname
+  attr_reader :maplocation
+  attr_reader :waypointName
+
+  def initialize(viewport=nil)
+    super(viewport)
+    @mapname     = ""
+    @maplocation = ""
+    @mapdetails  = ""
+    @waypointName = ""
+    @thisbitmap = BitmapWrapper.new(Graphics.width,Graphics.height)
+    pbSetSystemFont(@thisbitmap)
+    self.x = 0
+    self.y = 0
+    self.bitmap = @thisbitmap
+    refresh
+  end
+
+  def waypointName=(value)
+    if @waypointName != value
+      @waypointName = value
+      refresh
+    end
+  end
+
+  def refresh
+    self.bitmap.clear
+    textpos = [
+       [@mapname,18,-8,0,Color.new(248,248,248),Color.new(0,0,0)],
+       [@waypointName,Graphics.width-16,-8,1,Color.new(224,197,110),Color.new(0,0,0)],
+       [@maplocation,18,348,0,Color.new(248,248,248),Color.new(0,0,0)],
+       [@mapdetails,Graphics.width-16,348,1,Color.new(248,248,248),Color.new(0,0,0)]
+    ]
+    pbDrawTextPositions(self.bitmap,textpos)
+  end
 end
