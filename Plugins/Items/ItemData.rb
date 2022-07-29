dd6f36f8a7a365358958926a22f9e5233aececf7
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-07-29 12:38:54 -0700

    TM item descriptions are copied from their assigned moves

--- Plugins/Items/ItemData.rb
+++ Plugins/Items/ItemData.rb
@@ -1,5 +1,13 @@
 module GameData
     class Item
         def can_hold?;           return !is_important? && @pocket == 5; end
+
+        def description
+            if is_machine?
+                return pbGetMessage(MessageTypes::MoveDescriptions, GameData::Move.get(@move).id_number)
+            else
+                return pbGetMessage(MessageTypes::ItemDescriptions, @id_number)
+            end
+        end
     end
 end
