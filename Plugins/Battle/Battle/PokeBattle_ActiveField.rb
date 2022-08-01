a37def62d54fa2be5fc03d1289472bfc7e9012d9
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-07-31 23:00:56 -0700

    Bug fix: Initialize Puzzle Room

--- Plugins/Battle/Battle/PokeBattle_ActiveField.rb
+++ Plugins/Battle/Battle/PokeBattle_ActiveField.rb
@@ -16,6 +16,7 @@ class PokeBattle_ActiveField
       @effects[PBEffects::HappyHour]       = false
       @effects[PBEffects::IonDeluge]       = false
       @effects[PBEffects::MagicRoom]       = 0
+      @effects[PBEffects::PuzzleRoom]      = 0
       @effects[PBEffects::MudSportField]   = 0
       @effects[PBEffects::PayDay]          = 0
       @effects[PBEffects::TrickRoom]       = 0
