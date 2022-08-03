cacaae61a7f2f09539445dca4bd0e494eb7462bb
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-08-03 16:41:26 -0700

    Map secret settings changed for new graphics by ignited

--- Plugins/Miscellaneous/GlobalSettings.rb
+++ Plugins/Miscellaneous/GlobalSettings.rb
@@ -64,8 +64,8 @@ module Settings
   NUM_STORAGE_BOXES = 40
 
   REGION_MAP_EXTRAS = [
-    [0, 51, 9, 5, "mapHiddenCave", false],
-    [0, 52, 17, 13, "mapHidden2x1Route", false]
+    [0, 51, 9, 5, "Abyssal Cave", false],
+    [0, 52, 17, 13, "Volcanic Shore", false]
   ]
 
   # The names of each pocket of the Bag. Ignore the first entry ("").
