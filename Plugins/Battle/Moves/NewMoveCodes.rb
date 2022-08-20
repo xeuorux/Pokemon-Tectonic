dca90464852a7b20c24eb149774ef837b30b56b4
3rickbat <33363068+3rickbat@users.noreply.github.com>
2022-08-20 00:03:27 -0500

    roserade signature chuel whip

--- Plugins/Battle/Moves/NewMoveCodes.rb
+++ Plugins/Battle/Moves/NewMoveCodes.rb
@@ -2779,3 +2779,49 @@ class PokeBattle_Move_585 < PokeBattle_Move
 	  	@battle.pbDisplay(_INTL("{1} breaks open its witch powers!",user.pbThis))
 	end
 end
+
+#===============================================================================
+# Damaging move that sets Poison Spikes. (Cruel Whipblade)
+#===============================================================================
+class PokeBattle_Move_585 < PokeBattle_Move
+	def pbFailsAgainstTarget?(user,target)
+		return false if damagingMove?
+	end
+	
+	def pbEffectGeneral(user)
+		user.pbOpposingSide.effects[PBEffects::ToxicSpikes] += 1
+		if user.pbOpposingSide.effects[PBEffects::ToxicSpikes] > 2
+			user.pbOpposingSide.effects[PBEffects::ToxicSpikes] == 2
+			@battle.pbDisplay(_INTL("No new spikes were set since the opposing side already has two layers of poison spikes!"))
+			return false
+		end 
+        if user.pbOpposingSide.effects[PBEffects::ToxicSpikes] == 2
+            @battle.pbDisplay(_INTL("The second layer of poison spikes were scattered all around {1}'s feet!",
+			user.pbOpposingTeam(true)))
+        else
+            @battle.pbDisplay(_INTL("Poison spikes were scattered all around {1}'s feet!",
+			user.pbOpposingTeam(true)))
+        end
+        if user.pbOpposingSide.effects[PBEffects::FlameSpikes] > 0
+            user.pbOpposingSide.effects[PBEffects::FlameSpikes] = 0
+            @battle.pbDisplay(_INTL("The flame spikes around {1}'s feet were brushed aside!",
+                user.pbOpposingTeam(true)))
+        end
+        if user.pbOpposingSide.effects[PBEffects::FrostSpikes] > 0
+            user.pbOpposingSide.effects[PBEffects::FrostSpikes] = 0
+            @battle.pbDisplay(_INTL("The frost spikes around {1}'s feet were brushed aside!",
+                user.pbOpposingTeam(true)))
+        end
+    end
+
+    def getScore(score,user,target,skill=100)
+        if user.pbOpposingSide.effects[PBEffects::ToxicSpikes] >= 1
+            return 0
+        end
+        score -= 40
+        score += 10*@battle.pbAbleNonActiveCount(user.idxOpposingSide)
+        score += 10*@battle.pbAbleNonActiveCount(user.idxOwnSide)
+        return score
+    end
+
+end
