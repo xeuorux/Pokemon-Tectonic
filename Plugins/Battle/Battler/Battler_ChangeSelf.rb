b027121c99456b4864c14f10928d90635069217c
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-09-20 13:05:17 -0700

    INNERLIGHT given to Snivy line, new ability AGGRAVATE distributed
    Aggravate given to duskull line, chespin line, and trubbish line
    Trubbish line stats reworked to special and bulky again (Stunky is now the fast stench line)
    Trubbish line moveset reworked as special

--- Plugins/Battle/Battler/Battler_ChangeSelf.rb
+++ Plugins/Battle/Battler/Battler_ChangeSelf.rb
@@ -19,6 +19,7 @@ class PokeBattle_Battler
   def applyFractionalDamage(fraction,showDamageAnimation=true,basedOnCurrentHP=false)
     oldHP = @hp
     fraction /= 4.0 if boss?
+    fraction *= 2 if @battle.pbCheckOpposingAbility(:AGGRAVATE,@index)
     if basedOnCurrentHP
       reduction = (@hp * fraction).ceil
     else
