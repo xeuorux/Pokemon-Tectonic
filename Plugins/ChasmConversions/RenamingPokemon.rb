79b959d771f5bd3d1e5f5f7ea34333e7e5e4bb47
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-10-14 14:15:50 -0700

    Change existing save pokemon species ID for new unmega renames

---
+++ Plugins/ChasmConversions/RenamingPokemon.rb
@@ -0,0 +1,25 @@
+RENAMES_1_11 = {
+  :LAPNESSIE => :SAURENAID,
+  :ZWEIHANDOR => :REAVOR,
+}
+
+SaveData.register_conversion(:species_renaming_1_11) do
+  game_version '1.11.0'
+  display_title '1.11.0 species renames'
+  to_all do |save_data|
+    save_data[:player].party.each do |pokemon|
+        next if !RENAMES_1_11.has_key?(pokemon.species)
+        pokemon.species = RENAMES_1_11[pokemon.species]
+    end 
+    storage = save_data[:storage_system]
+    
+    for i in -1...storage.maxBoxes
+      for j in 0...storage.maxPokemon(i)
+        pokemon = storage.boxes[i][j]
+        if pokemon && RENAMES_1_11.has_key?(pokemon.species)
+          pokemon.species = RENAMES_1_11[pokemon.species]
+        end
+      end
+    end
+  end
+end
