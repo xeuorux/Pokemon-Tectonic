86661198bd838befa0b5a5efb39877332c71b465
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-10-17 23:34:50 -0700

    Championship tournament mapping, eventing, and systems code
    I've added placeholders for all the confirmed opponents thus far

---
+++ Plugins/Miscellaneous/zContent/Tournament.rb
@@ -0,0 +1,190 @@
+WIN_COUNT_VARIABLE = 28
+
+BLACKOUT_NURSE_EVENT_ID = 45
+BLACKOUT_NURSE_MAP_ID = 329
+
+# The position on the map enemy trainers should be found when you are going to battle them
+OPPONENT_MAP_POSITION = [22,16]
+
+
+# Trainer Type, name, version number, arena event ID
+POOL_1 = [
+    [:LEADER_Samorn_2,"Samorn",2,2],
+    [:LEADER_Lambert_2,"Lambert",2,3],
+    [:LEADER_Eko_2,"Eko",2,4],
+    [:COOLTRAINER_M7,"X",1,5],
+    [:FORMERCHAMP_Elise,"Elise",1,6],
+]
+
+POOL_2 = [
+    [:TRAINER_Alessa,"Alessa",3,7],
+    [:TRAINER_Eifion,"Eifion",1,8],
+    [:LEADER_Helena_2,"Helena",2,9],
+    [:LEADER_Bence_2,"Bence",2,10],
+]
+
+CHAMPION = [:TRAINER_Zain,"Zain",2,11]
+
+class PokemonGlobalMetadata
+    attr_accessor :tournament
+end
+
+class RandomTournament
+    attr_reader :matches
+    attr_reader :matchesWon
+
+    def initialize()
+        @matches = []
+        @matchesWon = 0
+    end
+
+    def resetTournament()
+        initialize()
+    end
+
+    def initializeTournament()
+        @matchesWon = 0
+        $game_variables[WIN_COUNT_VARIABLE] = 0
+
+        firstMatch = POOL_1.sample
+        secondMatch = nil
+        loop do
+            secondMatch = POOL_1.sample
+            break unless secondMatch == firstMatch
+        end
+
+        thirdMatch = POOL_2.sample
+        fourthMatch = nil
+        loop do
+            fourthMatch = POOL_2.sample
+            break unless fourthMatch == thirdMatch
+        end
+
+        fifthMatch = CHAMPION
+
+        @matches = [firstMatch,secondMatch,thirdMatch,fourthMatch,fifthMatch]
+    end
+
+    def winMatch()
+        @matchesWon += 1
+        $game_variables[WIN_COUNT_VARIABLE] = @matchesWon
+    end
+
+    def nextMatch()
+        return @matches[@matchesWon]
+    end
+
+    def tournamentBattle()
+        next_match = nextMatch()
+        return pbTrainerBattle(next_match[0],next_match[1],nil,false,next_match[2])
+    end
+
+    def opponentEvent()
+        return $game_system.map_interpreter.get_character(nextMatch()[3])
+    end
+
+    def prepareOpponent()
+        opponent_event = opponentEvent()
+        opponent_event.moveto(OPPONENT_MAP_POSITION[0],OPPONENT_MAP_POSITION[1])
+        opponent_event.turn_left()
+    end
+
+    def activateOpponent()
+       pbSetSelfSwitch(opponentEvent().id,'A')
+    end
+end
+
+def tournamentBattle()
+    return $PokemonGlobal.tournament.tournamentBattle()
+end
+
+def nextOpponentName()
+    return $PokemonGlobal.tournament.nextMatch()[1]
+end
+
+def winTournamentMatch()
+    $PokemonGlobal.tournament.winMatch()
+    pbMessage("The victor is \\PN!\\me[Bug catching 1st]")
+end
+
+def enterTournament()
+    $PokemonGlobal.tournament = RandomTournament.new if !$PokemonGlobal.tournament
+    $PokemonGlobal.tournament.initializeTournament()
+end
+
+def resetTournament()
+    $PokemonGlobal.tournament.resetTournament()
+end
+
+def promptForTournamentCommitment()
+    pbMessage(_INTL("The waiting room for tournament entrants is ahead."))
+    pbMessage(_INTL("Once you enter, you will not be able to save or change your team in any way until the tournament is complete."))
+    pbMessage(_INTL("A nurse is provided, however, for healing between matches."))
+    return pbConfirmMessageSerious(_INTL("Enter and begin the tournament?"))
+end
+
+def promptForTournamentQuit()
+    pbMessage(_INTL("If you leave, the tournament will be reset and your progress within it will be lost."))
+    return pbConfirmMessageSerious(_INTL("Would you still like to leave the tournament?"))
+end
+
+def promptForMatchCommitment()
+    pbMessage(_INTL("#{nextOpponentName} awaits you in the arena."))
+    return pbConfirmMessageSerious(_INTL("Are you ready to battle?"))
+end
+
+def handleMatchDecline()
+    pbMessage(_INTL("Ok, let me know when you are ready to battle #{nextOpponentName}."))
+end
+
+def alertNextMatch()
+    pbMessage(_INTL("Your next match will be against #{nextOpponentName}."))
+    pbMessage(_INTL("Return to me when you are ready to battle."))
+end
+
+def introduceMatch()
+    pbWait(20)
+    pbMessage(_INTL("The match between #{nextOpponentName} and \\PN will now begin!"))
+    pbWait(20)
+end
+
+def prepareOpponent()
+    $PokemonGlobal.tournament.prepareOpponent()
+end
+
+def activateOpponent()
+    $PokemonGlobal.tournament.activateOpponent()
+end
+
+def displayCurrentOdds()
+    displayRoundOdds($PokemonGlobal.tournament.matchesWon + 1)
+end
+
+def displayRoundOdds(round)
+    return if round < 1 || round > 6
+    if round == 6
+        pbMessage(_INTL("Odds are displayed for the grand final, gathered from a spectator poll."))
+        pbMessage(_INTL("Only 20 percent of respondents expect you to win against your brother."))
+    else
+        index = round-1
+        ordinal = ["second", "third", "fourth", "fifth"][index]
+        percent = [60,55,45,35][index]
+        pbMessage(_INTL("Odds are displayed for the #{ordinal} round matches, gathered from a spectator poll."))
+        pbMessage(_INTL("#{percent} percent of respondents expect you to win against #{nextOpponentName()}."))
+    end
+end
+
+def tournamentTrainerEnd(event,map_id)
+    pbTrainerEnd
+    pbSetSelfSwitch(event.id,'A',false,map_id)
+end
+
+def setCenterToBackupNurse
+    $PokemonGlobal.pokecenterMapId     = BLACKOUT_NURSE_MAP_ID
+    mapData = Compiler::MapData.new
+    map = mapData.getMap(BLACKOUT_NURSE_MAP_ID)
+    event = map.events[BLACKOUT_NURSE_EVENT_ID]
+    $PokemonGlobal.pokecenterX         = event.x
+    $PokemonGlobal.pokecenterY         = event.y + 1
+    $PokemonGlobal.pokecenterDirection = Up
+end
