c1cf2e1f68e1eb8d3764fe8713beefc02f645951
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-11-14 10:06:34 -0700

    In-game version to 1.12, save conversion to remove cut moves from existing saves

---
+++ Plugins/ChasmConversions/RemoveMoves.rb
@@ -0,0 +1,29 @@
+MOVES_REMOVED_112 = [:DISPLACE,:MESMERIZE,:COYGAZE,:MAJESTICGLARE]
+
+SaveData.register_conversion(:move_renaming_0) do
+  game_version '1.12.0'
+  display_title '1.12.0 move removals'
+  to_all do |save_data|
+    eachPokemonInSave(save_data) do |pokemon|
+      pokemon.moves.map! { |move|
+        moveID = move.id
+        if MOVES_REMOVED_112.include?(moveID)
+          echoln("Removing #{moveID} on player's #{pokemon.name}")
+          nil
+        else
+          move
+        end
+      }
+      pokemon.moves.compact!
+      pokemon.first_moves.map! { |move_id|
+        if MOVES_REMOVED_112.include?(move_id)
+          echoln("Removing #{move_id} on player's #{pokemon.name}'s first moves array")
+          nil
+        else
+          move_id
+        end
+      }
+      pokemon.first_moves.compact!
+    end
+  end
+end
--- Plugins/ChasmConversions/RenamingPokemon.rb
+++ Plugins/ChasmConversions/RenamingPokemon.rb
@@ -7,19 +7,23 @@ SaveData.register_conversion(:species_renaming_1_11) do
   game_version '1.11.0'
   display_title '1.11.0 species renames'
   to_all do |save_data|
-    save_data[:player].party.each do |pokemon|
-        next if !RENAMES_1_11.has_key?(pokemon.species)
-        pokemon.species = RENAMES_1_11[pokemon.species]
-    end 
-    storage = save_data[:storage_system]
-    
-    for i in -1...storage.maxBoxes
-      for j in 0...storage.maxPokemon(i)
-        pokemon = storage.boxes[i][j]
-        if pokemon && RENAMES_1_11.has_key?(pokemon.species)
-          pokemon.species = RENAMES_1_11[pokemon.species]
-        end
-      end
+    eachPokemonInSave(save_data) do |pokemon|
+      next if !RENAMES_1_11.has_key?(pokemon.species)
+      pokemon.species = RENAMES_1_11[pokemon.species]
+    end
+  end
+end
+
+def eachPokemonInSave(save_data)
+  save_data[:player].party.each do |pokemon|
+    yield pokemon
+  end 
+  storage = save_data[:storage_system]
+
+  for i in -1...storage.maxBoxes
+    for j in 0...storage.maxPokemon(i)
+      pokemon = storage.boxes[i][j]
+      yield pokemon if pokemon
     end
   end
 end
