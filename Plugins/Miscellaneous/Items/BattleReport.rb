e66b63cc158ecd5a2c74bf9f32f479606882817f
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2022-12-06 11:39:30 -0700

    Battle report no longer requires a confirmation to use

--- Plugins/Miscellaneous/Items/BattleReport.rb
+++ Plugins/Miscellaneous/Items/BattleReport.rb
@@ -1,13 +1,9 @@
 ItemHandlers::UseFromBag.add(:BATTLEREPORT,proc { |item|
-  if pbConfirmMessageSerious(_INTL("Read the battle report?"))
-    if !levelCapMaxed?
-      increaseLevelCap(5)
-    else
-      pbMessage(_INTL("You are too skilled to learn any more from a battle report."))
-      next 0
-    end
+  if !levelCapMaxed?
+    increaseLevelCap(5)
     next 3
   else
+    pbMessage(_INTL("You are too skilled to learn any more from a battle report."))
     next 0
   end
 })
