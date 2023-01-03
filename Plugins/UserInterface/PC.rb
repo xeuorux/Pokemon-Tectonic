f90ed5e5354f8f14d71f8f55c5de30b377d681de
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-01-03 09:48:06 -0700

    Added new "safe" object to player home that accesses item storage

--- Plugins/UserInterface/PC.rb
+++ Plugins/UserInterface/PC.rb
@@ -70,3 +70,59 @@ def pbPokeCenterPC
     end
     pbSEPlay("PC close")
 end
+
+def pbPCItemStorage
+    pbSEPlay("GUI storage pick up")
+    window = pbCreateMessageWindow()
+    command = 0
+    loop do
+      command = pbShowCommandsWithHelp(window,
+         [_INTL("Withdraw Item"),
+         _INTL("Deposit Item"),
+         _INTL("Toss Item"),
+         _INTL("Exit")],
+         [_INTL("Take out items from storage."),
+         _INTL("Store items in the safe."),
+         _INTL("Throw away items stored in the safe."),
+         _INTL("Do nothing.")],-1,command
+      )
+      case command
+      when 0   # Withdraw Item
+        if !$PokemonGlobal.pcItemStorage
+          $PokemonGlobal.pcItemStorage = PCItemStorage.new
+        end
+        if $PokemonGlobal.pcItemStorage.empty?
+          pbMessage(_INTL("There are no items."))
+        else
+          pbFadeOutIn {
+            scene = WithdrawItemScene.new
+            screen = PokemonBagScreen.new(scene,$PokemonBag)
+            screen.pbWithdrawItemScreen
+          }
+        end
+      when 1   # Deposit Item
+        pbFadeOutIn {
+          scene = PokemonBag_Scene.new
+          screen = PokemonBagScreen.new(scene,$PokemonBag)
+          screen.pbDepositItemScreen
+        }
+      when 2   # Toss Item
+        if !$PokemonGlobal.pcItemStorage
+          $PokemonGlobal.pcItemStorage = PCItemStorage.new
+        end
+        if $PokemonGlobal.pcItemStorage.empty?
+          pbMessage(_INTL("There are no items."))
+        else
+          pbFadeOutIn {
+            scene = TossItemScene.new
+            screen = PokemonBagScreen.new(scene,$PokemonBag)
+            screen.pbTossItemScreen
+          }
+        end
+      else
+        break
+      end
+    end
+    pbDisposeMessageWindow(window)
+    pbSEPlay("GUI storage put down")
+end
