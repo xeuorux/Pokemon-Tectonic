42a8c0a38d0c01438d8ce9d80b9f06695357a0be
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-01-17 14:27:34 -0700

    Delete CloudEncounters.rb

--- Plugins/WildPokemon/CloudEncounters.rb
+++
@@ -1,7 +0,0 @@
-def pbCloudRandomEncounter(chance = 20)
-    return unless get_self().moving? || $game_player.moving?
-    return unless rand(100) < chance
-    if $PokemonEncounters.encounter_triggered?(:Cloud, $PokemonGlobal.repel > 0, true)
-      pbEncounter(:Cloud)
-    end
-end
