ec0b290e43d667ffa8eb1aacd1bce8f272184dac
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-01-25 13:10:16 -0700

    Showcase screen improvements

--- Plugins/UserInterface/PartyShowcase.rb
+++ Plugins/UserInterface/PartyShowcase.rb
@@ -32,11 +32,11 @@ class PokemonPartyShowcase_Scene
         else
             fullDescription = "Tribes: " + fullDescription
         end
-        fullDescription = "<ac>#{fullDescription}</ac>"
         drawFormattedTextEx(@overlay, 8, Graphics.height - 20, Graphics.width, fullDescription, BASE_COLOR, SHADOW_COLOR)
 
         pbFadeInAndShow(@sprites) { pbUpdate }
-        pbScreenCapture(snapShotName)
+
+        pbScreenCapture(snapShotName) if snapshot
 
         loop do
             Graphics.update
@@ -51,8 +51,8 @@ class PokemonPartyShowcase_Scene
     end
 
     def renderShowcaseInfo(index,pokemon)
-        displayX =  ((index % 2) * (Graphics.width / 2)) + 4
-        displayY = (index / 2) * (Graphics.height / 3 - 8) + 4
+        displayX =  ((index % 2) * (Graphics.width / 2)) + 6
+        displayY = (index / 2) * (Graphics.height / 3 - 8) + 6
 
         mainIconY = displayY + 20
         newPokemonIcon = PokemonIconSprite.new(pokemon,@viewport)
@@ -62,7 +62,7 @@ class PokemonPartyShowcase_Scene
 
         # Display pokemon name
         nameAndLevel = pokemon.name + " Lv. " + pokemon.level.to_s
-        drawTextEx(@overlay, displayX + 4, displayY, 200, 1, nameAndLevel, BASE_COLOR, SHADOW_COLOR)
+        drawTextEx(@overlay, displayX + 14, displayY, 200, 1, nameAndLevel, BASE_COLOR, SHADOW_COLOR)
 
         # Display item icon
         if pokemon.item
@@ -77,7 +77,7 @@ class PokemonPartyShowcase_Scene
         # Display moves
         pokemon.moves.each_with_index do |pokemonMove,moveIndex|
             moveName = GameData::Move.get(pokemonMove.id).real_name
-            drawTextEx(@overlay, displayX + POKEMON_ICON_SIZE + 8, mainIconY + moveIndex * 16, 200, 1, moveName, BASE_COLOR, SHADOW_COLOR)
+            drawTextEx(@overlay, displayX + POKEMON_ICON_SIZE + 8, mainIconY + 2 + moveIndex * 16, 200, 1, moveName, BASE_COLOR, SHADOW_COLOR)
         end
 
         # Display ability name
@@ -85,11 +85,11 @@ class PokemonPartyShowcase_Scene
         drawTextEx(@overlay, displayX + 4, mainIconY + POKEMON_ICON_SIZE + 8, 200, 1, abilityName, BASE_COLOR, SHADOW_COLOR)
     
         # Display style values
-        styleValueX = displayX + 224
+        styleValueX = displayX + 222
         styleHash = pokemon.ev
         styleValues = [styleHash[:HP],styleHash[:ATTACK],styleHash[:DEFENSE],styleHash[:SPECIAL_ATTACK],styleHash[:SPECIAL_DEFENSE],styleHash[:SPEED]]
         styleValues.each_with_index do |styleValue,styleIndex|
-            drawTextEx(@overlay, styleValueX, displayY + 16 * styleIndex, 80, 1, styleValue.to_s, BASE_COLOR, SHADOW_COLOR)
+            drawTextEx(@overlay, styleValueX, 4 + displayY + 16 * styleIndex, 80, 1, styleValue.to_s, BASE_COLOR, SHADOW_COLOR)
         end
     end
 
