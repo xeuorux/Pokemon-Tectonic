b2f78b99380420ad87a15cfcb79f6751a87d0ff1
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-02-02 12:56:20 -0700

    Text entry for sliders in the animation editor
    Also made the slider change speed increase even more when held for 4+ seconds

--- Plugins/Miscellaneous/AnimationEditorTweaks.rb
+++ Plugins/Miscellaneous/AnimationEditorTweaks.rb
@@ -1,3 +1,112 @@
+class Slider < UIControl
+  def initialize(label,minvalue,maxvalue,curval)
+    super(label)
+    @minvalue = minvalue
+    @maxvalue = maxvalue
+    @curvalue = curval
+    @label = label
+    @leftarrow = Rect.new(0,0,0,0)
+    @rightarrow = Rect.new(0,0,0,0)
+    @labelclickable = Rect.new(0,0,0,0)
+    self.minvalue = minvalue
+    self.maxvalue = maxvalue
+    self.curvalue = curval
+    @textEnterable = true
+  end
+
+  # By how much the slider should change per frame at the given number of milliseconds of holding down the mouse
+  def getChangePerRepeatTime(repeattime)
+    if repeattime > 4000
+      return 30
+    elsif repeattime > 2500
+      return 15
+    elsif repeattime > 1250
+      return 5
+    end
+    return 1
+  end
+
+  def update
+    mousepos=Mouse::getMousePos
+    self.changed=false
+    if self.minvalue<self.maxvalue && self.curvalue<self.minvalue
+      self.curvalue=self.minvalue
+    end
+    return false if self.disabled
+    return false if !Input.repeat?(Input::MOUSELEFT)
+    return false if !mousepos
+    left = toAbsoluteRect(@leftarrow)
+    right = toAbsoluteRect(@rightarrow)
+    labelclickable = toAbsoluteRect(@labelclickable)
+    oldvalue = self.curvalue
+    repeattime = Input.time?(Input::MOUSELEFT) / 1000
+    # Left arrow
+    if left.contains(mousepos[0],mousepos[1])
+      self.curvalue -= getChangePerRepeatTime(repeattime)
+      self.curvalue = self.curvalue.floor
+      self.changed = (self.curvalue != oldvalue)
+      self.invalidate
+    end
+    #Right arrow
+    if right.contains(mousepos[0],mousepos[1])
+      self.curvalue += getChangePerRepeatTime(repeattime)
+      self.curvalue = self.curvalue.floor
+      self.changed = (self.curvalue != oldvalue)
+      self.invalidate
+    end
+    #Clicking on label
+    if labelclickable.contains(mousepos[0],mousepos[1])
+      maxDigits = maxvalue.digits.length
+      text = pbEnterText(_INTL("Enter slider value."),0,maxDigits,"",0,nil,true)
+      unless text.blank?
+        begin
+          enteredValue = Integer(text || '')
+          if enteredValue >= self.minvalue && enteredValue <= self.maxvalue
+            self.curvalue = enteredValue
+            self.curvalue = self.curvalue.floor
+            self.changed = (self.curvalue != oldvalue)
+            self.invalidate
+          else
+            pbMessage(_INTL("Entered value doesn't fit within the slider's bounds."))
+          end
+        rescue ArgumentError
+          pbMessage(_INTL("Entered value is not a valid integer."))
+        end
+      end
+    end
+  end
+
+  def refresh
+    bitmap=self.bitmap
+    x=self.x
+    y=self.y
+    width=self.width
+    height=self.height
+    color=Color.new(120,120,120)
+    bitmap.fill_rect(x,y,width,height,Color.new(0,0,0,0))
+    size=bitmap.text_size(self.label).width
+    leftarrows=bitmap.text_size(_INTL(" << "))
+    numbers=bitmap.text_size(" XXXX ").width
+    rightarrows=bitmap.text_size(_INTL(" >> "))
+    bitmap.font.color=color
+    shadowtext(bitmap,x,y,size,height,self.label)
+    x+=size
+    shadowtext(bitmap,x,y,leftarrows.width,height,_INTL(" << "),
+       self.disabled || self.curvalue==self.minvalue)
+    @leftarrow=Rect.new(x,y,leftarrows.width,height)
+    x+=leftarrows.width
+    @labelclickable=Rect.new(x,y,numbers,height)
+    if !self.disabled
+      bitmap.font.color=color
+      shadowtext(bitmap,x,y,numbers,height," #{self.curvalue} ",false,1)
+    end
+    x+=numbers
+    shadowtext(bitmap,x,y,rightarrows.width,height,_INTL(" >> "),
+       self.disabled || self.curvalue==self.maxvalue)
+    @rightarrow=Rect.new(x,y,rightarrows.width,height)
+  end
+end
+
 def pbAnimList(animations,canvas,animwin)
     commands=[]
     for i in 0...animations.length
