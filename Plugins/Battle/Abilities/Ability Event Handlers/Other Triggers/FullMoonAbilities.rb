bec43b622c3f8acd7e67b858de21888c010b64dc
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-03-08 19:33:42 -0700

    WEREWOLF recode

--- Plugins/Battle/Abilities/Ability Event Handlers/Other Triggers/FullMoonAbilities.rb
+++ Plugins/Battle/Abilities/Ability Event Handlers/Other Triggers/FullMoonAbilities.rb
@@ -2,10 +2,10 @@
 
 BattleHandlers::FullMoonAbility.add(:WEREWOLF,
     proc { |_ability, battler, _battle|
-        next unless user.species == :LYCANROC
-        next unless user.form == 0
-        battle.pbShowAbilitySplash(user)
-        user.pbChangeForm(1, _INTL("{1}'s transforms with exposure to the Full Moon!", user.pbThis))
-        battle.pbHideAbilitySplash(user)
+        next unless battler.species == :LYCANROC
+        next unless battler.form == 0
+        battle.pbShowAbilitySplash(battler)
+        battler.pbChangeForm(1, _INTL("{1}'s transforms with exposure to the Full Moon!", battler.pbThis))
+        battle.pbHideAbilitySplash(battler)
     }
 )
