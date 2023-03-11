ab9ae8b8c41d64bc386c33dd2da3eea76af3e5bf
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-03-10 23:07:38 -0700

    Fix issue with wild battle start animations while surfing

--- Plugins/Battle/BattleStarting/Overworld_BattleIntroAnim.rb
+++ Plugins/Battle/BattleStarting/Overworld_BattleIntroAnim.rb
@@ -79,8 +79,7 @@ def pbBattleAnimation(bgm=nil,battletype=0,foe=nil)
     end
     # Play main animation
     Graphics.freeze
-	  rate = location == 3 ? 1.25 : 1.0
-    rate /= 1.5 if fastTransitions?
+	  rate = fastTransitions? ? 0.6 : 1.0
     Graphics.transition(Graphics.frame_rate*rate,sprintf("Graphics/Transitions/%s",anim))
     viewport.color = Color.new(0,0,0,255)   # Ensure screen is black
     # Slight pause after animation before starting up the battle scene
@@ -117,4 +116,32 @@ def pbBattleAnimation(bgm=nil,battletype=0,foe=nil)
   end
   viewport.dispose
   $game_temp.in_battle = false
+end
+
+module Transitions
+  class RisingSplash
+    def update
+      return if disposed?
+      if @duration==0
+        dispose
+      else
+        angadd = (@numframes-@duration)*@angmult
+        amp = 6*angadd/8; amp = 6 if amp>6
+        for i in 0...@bgsprites.length
+          @bgsprites[i].x = amp*Math.sin((i+angadd)*Math::PI/10)
+        end
+        @bubblesprite.x = (@width-@bubblebitmap.width)/2
+        @bubblesprite.x -= 32*Math.sin((@numframes-@duration)/(@numframes/50.0)*3*Math::PI/60)
+        @bubblesprite.y -= @bubblesuby
+        if @duration<@numframes*0.5
+          @splashsprite.y -= @splashsuby
+        end
+        if @duration<@numframes*0.1
+          @blacksprite.y -= @blacksuby
+          @blacksprite.y = 0 if @blacksprite.y<0
+        end
+      end
+      @duration -= 1
+    end
+  end
 end
