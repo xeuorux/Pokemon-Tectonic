870cf45aad1c3e522f56ad456dc5daa66367a9ca
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-03-12 19:04:20 -0700

    Remove extra "spikes" announcement for curse

--- Plugins/Battle/AI/Trainers AI/Curses/Spikes.rb
+++ Plugins/Battle/AI/Trainers AI/Curses/Spikes.rb
@@ -1,6 +1,5 @@
 PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_SPIKES,
     proc { |curse_policy, battle, curses_array|
-        battle.amuletActivates("Spikes")
         spikesCount = battle.sides[0].incrementEffect(:Spikes, GameData::BattleEffect.get(:Spikes).maximum)
         explanation = _INTL("#{spikesCount} layers of Spikes were scattered around you!")
         battle.amuletActivates(
