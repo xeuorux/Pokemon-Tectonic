fe30f4ba79127f88a8d2c1c5f24166765976cbb0
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-03-16 15:38:04 -0700

    Mark accuracy reducing moves as cut, replace MIRRORSHOT, debug command for listing cut moves

---
+++ Plugins/Miscellaneous/Debug Commands/Analysis/ListCutMoves.rb
@@ -0,0 +1,26 @@
+DebugMenuCommands.register("listnewmoves", {
+  "parent"      => "analysis",
+  "name"        => _INTL("List new moves"),
+  "description" => _INTL("List all new moves added to the game."),
+  "effect"      => proc { |sprites, viewport|
+	
+	moveDataSorted = []
+	GameData::Move.each do |moveData|
+		next unless moveData.tectonic_new
+		moveDataSorted.push(moveData)
+	end
+
+	moveDataSorted.sort_by! { |data|
+		GameData::Type.get(data.type).id_number * 10_000 + data.category * 1000 + data.base_damage
+	}
+
+	File.open("new_moves.txt","wb") { |file|
+		moveDataSorted.each do |moveData|
+			moveLine = moveData.real_name
+			moveLine += "\r\n"
+			file.write(moveLine)
+		end
+	}
+	pbMessage(_INTL("Cut moves information written to cut_moves.txt"))
+  }
+})
