372a3ef46ade13afad467e7a011d1e1c76f45fc7
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-03-21 20:19:52 -0700

    CANDYs and VANILLATUMULAs can now be used multiple at a time

--- Plugins/Miscellaneous/Items/Candy.rb
+++ Plugins/Miscellaneous/Items/Candy.rb
@@ -1,15 +1,40 @@
 ItemHandlers::UseOnPokemon.add(:RARECANDY,proc { |item,pkmn,scene|
-  if pkmn.level>=GameData::GrowthRate.max_level || pkmn.shadowPokemon?
+  pbLevelGivingItem(pkmn, item, scene)
+})
+
+
+def pbLevelGivingItem(pkmn, item, scene)
+  if pkmn.level >= GameData::GrowthRate.max_level || pkmn.shadowPokemon?
     scene.pbDisplay(_INTL("It won't have any effect."))
-    next false
+    return false
   elsif LEVEL_CAPS_USED && (pkmn.level + 1) > $game_variables[26]
       scene.pbDisplay(_INTL("It won't have any effect due to the level cap at #{$game_variables[26]}."))
-      next false
+      return false
   end
-  pbChangeLevel(pkmn,pkmn.level+1,scene)
+
+  # Ask the player how many they'd like to apply
+  level_cap = LEVEL_CAPS_USED ? $game_variables[26] : growth_rate.max_level
+  maxLevelIncrease = level_cap - pkmn.level
+  maximum = [maxLevelIncrease, $PokemonBag.pbQuantity(item)].min # Max items which can be used
+  if maximum > 1
+      params = ChooseNumberParams.new
+      params.setRange(1, maximum)
+      params.setInitialValue(1)
+      params.setCancelValue(0)
+      question = _INTL("How many {1} do you want to use?", GameData::Item.get(item).name_plural)
+      qty = pbMessageChooseNumber(question, params)
+  else
+      qty = 1
+  end
+
+  return false if qty < 1
+
+  $PokemonBag.pbDeleteItem(item, qty - 1)
+  pbChangeLevel(pkmn,pkmn.level + qty,scene)
   scene.pbHardRefresh
-  next true
-})
+
+  return true
+end
 
 ItemHandlers::UseOnPokemon.copy(:RARECANDY,:VANILLATULUMBA)
 
