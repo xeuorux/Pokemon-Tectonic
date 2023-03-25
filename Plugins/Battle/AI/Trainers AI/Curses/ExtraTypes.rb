4b125f5509e2be33a75ca8e89c983bbdd86ef469
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-03-25 10:42:22 -0700

    CURSE_EXTRA_TYPES replaces existing curse on Noel

---
+++ Plugins/Battle/AI/Trainers AI/Curses/ExtraTypes.rb
@@ -0,0 +1,35 @@
+PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_EXTRA_TYPES,
+    proc { |curse_policy, battle, curses_array|
+        battle.amuletActivates(
+            _INTL("TODO"),
+            _INTL("Enemy Pokemon all have an extra type.")
+        )
+        curses_array.push(curse_policy)
+        next curses_array
+    }
+)
+
+PokeBattle_Battle::BattlerEnterCurseEffect.add(:CURSE_EXTRA_TYPES,
+    proc { |_curse_policy, battler, battle|
+        next unless battler.opposes?
+
+        type = nil
+        case battler.species
+        when :MRRIME
+            type = :FAIRY
+        when :METAGROSS
+            type = :FLYING
+        when :LUNATONE
+            type = :ELECTRIC
+        when :CRABOMINABLE
+            type = :DARK
+        when :GLALIE
+            type = :GHOST
+        when :GIRAFARIG
+            type = :STEEL
+        end
+        battler.effects[:Type3] = type
+
+        battle.scene.pbRefresh
+    }
+)
