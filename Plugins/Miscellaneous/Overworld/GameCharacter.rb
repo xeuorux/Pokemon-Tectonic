4bf9d4f313832ce7f1ffe77e517ff9bf935a7f86
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-04-01 07:24:59 -0700

    Follower pokemon jumping crash potential fix

--- Plugins/Miscellaneous/Overworld/GameCharacter.rb
+++ Plugins/Miscellaneous/Overworld/GameCharacter.rb
@@ -1,6 +1,16 @@
 class Game_Character
-    def direction
-        #@direction = 2 if @direction.nil?
-        return @direction
+    def screen_y
+        ret = screen_y_ground
+        if jumping?
+          if @jump_count > 0
+            jump_fraction = ((@jump_count * jump_speed_real / Game_Map::REAL_RES_X) - 0.5).abs   # 0.5 to 0 to 0.5
+          elsif @jump_distance > 0
+            jump_fraction = ((@jump_distance_left / @jump_distance) - 0.5).abs   # 0.5 to 0 to 0.5
+          else
+            jump_fraction = 0.5
+          end
+          ret += @jump_peak * (4 * jump_fraction**2 - 1)
+        end
+        return ret
     end
 end
