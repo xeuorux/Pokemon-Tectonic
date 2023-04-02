2328bc5c197d8a5358689574b35f1ebf9805bf5d
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-04-02 07:54:38 -0700

    Added and distributed DISABLEBALL, LEECHBALL, and POTIONBALL
    Removed NETBALL, DUSKBALL, LEVELBALL from the sweetrock harbor vendor

--- Plugins/Battle/Items/PokeBalls/OnFailCatchHandlers.rb
+++ Plugins/Battle/Items/PokeBalls/OnFailCatchHandlers.rb
@@ -1,3 +1,15 @@
 BallHandlers::OnFailCatch.add(:SLICEBALL, proc { |_ball, _battle, battler|
     battler.applyFractionalDamage(1/4.0)
 })
+
+BallHandlers::OnFailCatch.add(:LEECHBALL, proc { |_ball, _battle, battler|
+    battler.applyLeeched if battler.canBeLeeched?
+})
+
+BallHandlers::OnFailCatch.add(:POTIONBALL, proc { |_ball, _battle, battler|
+    battler.applyFractionalHealing(1/4.0)
+})
+
+BallHandlers::OnFailCatch.add(:DISABLEBALL, proc { |_ball, _battle, battler|
+    battler.applyEffect(:Disable) if target.canBeDisabled?(true)
+})
