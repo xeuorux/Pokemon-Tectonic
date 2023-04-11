452eb282ecd6d39d283b20b5f652cf2417f247ae
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-04-11 09:40:57 -0700

    Steaming particle engines for boiling cave and abyssal cavern

--- Plugins/Miscellaneous/Graphics/ParticleEngine/ParticleEngine.rb
+++ Plugins/Miscellaneous/Graphics/ParticleEngine/ParticleEngine.rb
@@ -30,6 +30,8 @@ class Particle_Engine
            # By Zeu
            "starfield"    => Particle_Engine::CircleStarField,
            "wormhole"     => Particle_Engine::Wormhole,
+           "steamy"       => Particle_Engine::Steamy,
+           "steamy2"      => Particle_Engine::Steamy2,
         }
     end
 end
@@ -175,7 +177,7 @@ class ParticleEffect_Event
             minX -= @bmwidth
             minY -= @bmheight
             maxX += @bmwidth
-            maxY += @bmheight            
+            maxY += @bmheight
 
             if maxX < 0 || maxY < 0 || minX >= Graphics.width || minY >= Graphics.height
                 return
@@ -337,11 +339,11 @@ class ParticleSprite
     end
   
     def update
-      w = Graphics.width
-      h = Graphics.height
-      if !@sprite && @x>=@minleft && @y>=@mintop && @x<w && @y<h
+      w = Graphics.width + @bitmap.width
+      h = Graphics.height + @bitmap.height
+      if !@sprite && @x >= @minleft && @y >= @mintop && @x < w && @y < h
         @sprite = Sprite.new(@viewport)
-      elsif @sprite && (@x<@minleft || @y<@mintop || @x>=w || @y>=h)
+      elsif @sprite && (@x < @minleft || @y < @mintop || @x >= w || @y >= h)
         @sprite.dispose
         @sprite = nil
       end
@@ -505,5 +507,166 @@ class Particle_Engine::Wormhole < ParticleEffect_Event
             @particles[i].angle -= 1
         end
         super
+    end
+end
+
+class Particle_Engine::Steamy < ParticleEffect_Event
+    def initialize(event,viewport)
+        super
+        setParameters([
+            0, # Random hue
+            0, # fade
+            10, # max particles
+            0, # hue
+            5, # slowdown
+            -Graphics.height, # ytop
+            Graphics.height, # ybottom
+            0, # xleft
+            Graphics.width, # xright
+            0, # xgravity
+            -0.5, # ygravity
+            0, # xoffset
+            0, # yoffset
+            3, # opacity var
+            5 # original opacity
+            ])
+        @opacityMult = 0.1
+        @huerange = 10
+        @cullOffscreen = false
+        @movesleftright = false
+        @movesupdown = false
+        @radius = 100
+        @rad2 = @radius * @radius
+        @maxOpacity = 80
+
+        initParticles("steam",100)
+    end
+
+    def resetParticle(i)
+        randomRad = Math.sqrt(rand(@rad2))
+        randomAngle = rand(360)
+        xRand = Math.cos(randomAngle) * randomRad
+        yRand = Math.sin(randomAngle) * randomRad
+        @particles[i].x = @startingx + @xoffset + xRand
+        @particles[i].y = @startingy + @yoffset + yRand
+        @particlex[i] = xRand
+        @particley[i] = yRand
+        @particles[i].state = 0
+
+        hue = @hue + rand(@huerange) - @huerange / 2
+        hue -= 360 if @hue >= 360
+        hue += 360 if @hue <= 0
+
+        @particles[i].bitmap = loadBitmap(@filename, hue)
+    end
+
+    def resetOpacity(i)
+        @opacity[i] = 1
+        @particles[i].state = 0
+    end
+
+    def initializeParticle(i)
+        @opacity[i] = rand(@maxOpacity)
+        @particles[i].angle = rand(360)
+    end
+
+    def changeOpacity(i)
+        if @particles[i].state == 0
+            @opacity[i] += 1
+            if @opacity[i] >= @maxOpacity
+                @particles[i].state = 1
+            end
+        else
+            @opacity[i] -= 1
+        end
+    end
+
+    def xExtent
+        return @radius * 2
+    end
+
+    def yExtent
+        return @radius * 2
+    end
+end
+
+class Particle_Engine::Steamy2 < ParticleEffect_Event
+    def initialize(event,viewport)
+        super
+        setParameters([
+            0, # Random hue
+            0, # fade
+            20, # max particles
+            0, # hue
+            5, # slowdown
+            -Graphics.height, # ytop
+            Graphics.height, # ybottom
+            0, # xleft
+            Graphics.width, # xright
+            0, # xgravity
+            -0.5, # ygravity
+            0, # xoffset
+            0, # yoffset
+            3, # opacity var
+            5 # original opacity
+            ])
+        @opacityMult = 0.2
+        @huerange = 10
+        @cullOffscreen = false
+        @movesleftright = false
+        @movesupdown = false
+        @radius = 50
+        @rad2 = @radius * @radius
+        @maxOpacity = 50
+
+        initParticles("steam",100)
+    end
+
+    def resetParticle(i)
+        randomRad = Math.sqrt(rand(@rad2))
+        randomAngle = rand(360)
+        xRand = Math.cos(randomAngle) * randomRad
+        xRand *= 1.5
+        yRand = Math.sin(randomAngle) * randomRad
+        @particles[i].x = @startingx + @xoffset + xRand
+        @particles[i].y = @startingy + @yoffset + yRand
+        @particlex[i] = xRand
+        @particley[i] = yRand
+        @particles[i].state = 0
+
+        hue = @hue + rand(@huerange) - @huerange / 2
+        hue -= 360 if @hue >= 360
+        hue += 360 if @hue <= 0
+
+        @particles[i].bitmap = loadBitmap(@filename, hue)
+    end
+
+    def resetOpacity(i)
+        @opacity[i] = 1
+        @particles[i].state = 0
+    end
+
+    def initializeParticle(i)
+        @opacity[i] = rand(@maxOpacity)
+        @particles[i].angle = rand(360)
+    end
+
+    def changeOpacity(i)
+        if @particles[i].state == 0
+            @opacity[i] += 1
+            if @opacity[i] >= @maxOpacity
+                @particles[i].state = 1
+            end
+        else
+            @opacity[i] -= 1
+        end
+    end
+
+    def xExtent
+        return @radius * 2
+    end
+
+    def yExtent
+        return @radius * 2
     end
 end
