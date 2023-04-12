3a50335fc79b6bb2b7237a76dcafb3b347aba049
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-04-12 00:00:46 -0700

    Fix UNCONCERNED typo

--- Plugins/Battle/Abilities/Ability Event Handlers/Damage/DamageCalcTargetAbilities.rb
+++ Plugins/Battle/Abilities/Ability Event Handlers/Damage/DamageCalcTargetAbilities.rb
@@ -198,7 +198,7 @@ BattleHandlers::DamageCalcTargetAbility.add(:FEATHERCOAT,
   }
 )
 
-BattleHandlers::DamageCalcTargetAbility.add(:UNCONCERED,
+BattleHandlers::DamageCalcTargetAbility.add(:UNCONCERNED,
   proc { |ability, _user, target, _move, mults, _baseDmg, _type|
       mults[:final_damage_multiplier] *= 0.8 if Effectiveness.normal?(target.damageState.typeMod)
   }
