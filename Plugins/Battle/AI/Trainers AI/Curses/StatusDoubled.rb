454dd160ab8b303538b0ce4f9e22c4ddb1021a88
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-04-13 19:09:48 -0700

    Update StatusDoubled.rb

--- Plugins/Battle/AI/Trainers AI/Curses/StatusDoubled.rb
+++ Plugins/Battle/AI/Trainers AI/Curses/StatusDoubled.rb
@@ -2,7 +2,7 @@ PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_STATUS_DOUBLED,
     proc { |curse_policy, battle, curses_array|
         battle.amuletActivates(
             _INTL("Pyres raised\nHere we meet\nSpirits razed\nReduced to meat"),
-            _INTL("Status conditions affect your Pokemon twice as much!")
+            _INTL("Status conditions ignore Safeguard and affect your Pokemon twice as much!")
         )
         curses_array.push(curse_policy)
         next curses_array
