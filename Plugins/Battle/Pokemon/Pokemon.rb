70e1b06f2162f9e6242b5a86bd63ae45223aa325
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-04-20 13:55:06 -0700

    TM fix

--- Plugins/Battle/Pokemon/Pokemon.rb
+++ Plugins/Battle/Pokemon/Pokemon.rb
@@ -888,7 +888,8 @@ class Pokemon
     # @return [Boolean] whether the Pokémon is compatible with the given move
     def compatible_with_move?(move_id)
       move_data = GameData::Move.try_get(move_id)
-      return move_data && species_data.tutor_moves.include?(move_data.id)
+      return false if move_data.nil?
+      return learnable_moves.include?(move_data.id)
     end
 
     # def can_relearn_move?
