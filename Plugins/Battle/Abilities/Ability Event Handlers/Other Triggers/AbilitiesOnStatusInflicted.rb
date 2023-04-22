94eb9b413277b256eb89b738cdc20e3f30bdf491
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-04-22 12:02:25 -0700

    SYNCHRONIZE does all statuses, description update

--- Plugins/Battle/Abilities/Ability Event Handlers/Other Triggers/AbilitiesOnStatusInflicted.rb
+++ Plugins/Battle/Abilities/Ability Event Handlers/Other Triggers/AbilitiesOnStatusInflicted.rb
@@ -2,24 +2,9 @@ BattleHandlers::AbilityOnStatusInflicted.add(:SYNCHRONIZE,
     proc { |ability, battler, user, status|
         next if !user || user.index == battler.index
         next unless user.pbCanSynchronizeStatus?(status, battler)
-        case status
-        when :POISON
-            battler.battle.pbShowAbilitySplash(battler, ability)
-            user.applyPoison(battler)
-            battler.battle.pbHideAbilitySplash(battler)
-        when :BURN
-            battler.battle.pbShowAbilitySplash(battler, ability)
-            user.applyBurn(battler)
-            battler.battle.pbHideAbilitySplash(battler)
-        when :NUMB
-            battler.battle.pbShowAbilitySplash(battler, ability)
-            user.applyNumb(battler)
-            battler.battle.pbHideAbilitySplash(battler)
-        when :FROSTBITE
-            battler.battle.pbShowAbilitySplash(battler, ability)
-            user.applyFrostbite(battler)
-            battler.battle.pbHideAbilitySplash(battler)
-        end
+        battler.battle.pbShowAbilitySplash(battler, ability)
+        user.pbInflictStatus(status)
+        battler.battle.pbHideAbilitySplash(battler)
     }
 )
 
