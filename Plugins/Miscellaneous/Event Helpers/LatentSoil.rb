c1bf079b4d0510d686d50cd0824500278cb7364d
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-05-05 00:53:53 -0700

    Celebi Quest tweaks and fixes

--- Plugins/Miscellaneous/Event Helpers/LatentSoil.rb
+++ Plugins/Miscellaneous/Event Helpers/LatentSoil.rb
@@ -4,6 +4,8 @@ def latentSoil
         if pbConfirmMessage(_INTL("Plant a Latent Seed?"))
             $PokemonBag.pbDeleteItem(:LATENTSEED)
             $game_variables[31] += 1 # Latent Seeds planted
+            pbMessage(_INTL("Right as you push the seed into the soil, a sapling bursts out of it!"))
+            pbSEPlay("Anim/PRSFX- Growth",60,110)
             return true
         else
             return false
