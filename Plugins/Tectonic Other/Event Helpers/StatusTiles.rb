5295164ab744e9f1c5b5ba08703cce77b82802a5
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-05-19 19:27:12 -0700

    Tile that cleanses party of status

--- Plugins/Tectonic Other/Event Helpers/StatusTiles.rb
+++ Plugins/Tectonic Other/Event Helpers/StatusTiles.rb
@@ -50,3 +50,21 @@ end
 def inflictLeeched
     inflictFirstAble(:LEECHED)
 end
+
+def cleanseWholeParty
+    $Trainer.able_party.each do |partyMember|
+        next if partyMember.status == :NONE
+
+        case status
+        when :SLEEP			then pbMessage(_INTL("{1} woke up!", partyMember.name))
+        when :POISON		then pbMessage(_INTL("{1} was cured of its poisoning.", partyMember.name))
+        when :BURN	        then pbMessage(_INTL("{1}'s burn was healed.", partyMember.name))
+        when :FROSTBITE     then pbMessage(_INTL("{1}'s frostbite was healed.", partyMember.name))
+        when :NUMB 			then pbMessage(_INTL("{1} is no longer numbed.", partyMember.name))
+        when :DIZZY			then pbMessage(_INTL("{1} is no longer dizzy!", partyMember.name))
+        when :LEECHED       then pbMessage(_INTL("{1} is no longer being leeched!", partyMember.name))
+        end
+
+        partyMember.heal_status
+    end
+end
