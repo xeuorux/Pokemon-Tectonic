917489535d6b4ecb8d2ec2e598344ccc1ce02de3
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-05-26 13:39:09 -0700

    Slight pro trainer tweaks
    A bit of prep for the tapu island, too

--- Plugins/Tectonic Other/BoatTeleports/BoatTeleports.rb
+++ Plugins/Tectonic Other/BoatTeleports/BoatTeleports.rb
@@ -2,6 +2,7 @@ CASABA_VILLA_DOCK = _INTL("Casaba Villa")
 MAINLAND_DOCK = _INTL("Feebas' Fin")
 ELEIG_BOATING_DOCK = _INTL("Eleig Boating Dock")
 SWEETROCK_DOCK = _INTL("Sweetrock Harbor")
+TAPU_ISLAND = _INTL("To Be Named")
 
 def boatTravel(currentDock = "")
     casabaVillaCommand = -1
@@ -14,6 +15,7 @@ def boatTravel(currentDock = "")
     commands[mainlandDockCommand = commands.length] = _INTL(MAINLAND_DOCK) if currentDock != MAINLAND_DOCK
     commands[eleigBoatingCommand = commands.length] = _INTL(ELEIG_BOATING_DOCK) if $game_switches[70] && currentDock != ELEIG_BOATING_DOCK
     commands[sweetrockHarborCommand = commands.length] = _INTL(SWEETROCK_DOCK) if $game_switches[71] && currentDock != SWEETROCK_DOCK
+    commands[sweetrockHarborCommand = commands.length] = _INTL(TAPU_ISLAND) if $game_switches[78] && currentDock != TAPU_ISLAND
     commands.push(_INTL("Cancel"))
 
     choice = pbMessage(_INTL("Where would you like to go?"),commands,commands.length)
