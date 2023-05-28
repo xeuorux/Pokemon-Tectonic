82d03f3b7dbba484dde7785cf986a8b813e77c66
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-05-28 16:34:20 -0700

    Update OverworldTriggers.rb

--- Plugins/Tectonic Other/Overworld/OverworldTriggers.rb
+++ Plugins/Tectonic Other/Overworld/OverworldTriggers.rb
@@ -12,6 +12,7 @@ end
 #===============================================================================
 # Party Pokémon gain happiness from walking
 Events.onStepTaken += proc {
+  next if $Trainer.able_pokemon_count # If has no party (e.g. in debug mode)
   $PokemonGlobal.happinessSteps = 0 if !$PokemonGlobal.happinessSteps
   $PokemonGlobal.happinessSteps += 1
   threshold = (20 * 6) / $Trainer.able_pokemon_count # The more pokemon, the more often gains happiness
