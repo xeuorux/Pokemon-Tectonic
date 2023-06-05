6e68899df52a923377c97c95c390fa1082af2c1d
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-06-05 00:14:45 -0700

    Curse names to replace the "TODO"s

--- Plugins/Tectonic Battle/AI/Trainers AI/Curses/DoubleAbilities.rb
+++ Plugins/Tectonic Battle/AI/Trainers AI/Curses/DoubleAbilities.rb
@@ -1,7 +1,7 @@
 PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_DOUBLE_ABILITIES,
     proc { |curse_policy, battle, curses_array|
         battle.amuletActivates(
-            _INTL("TODO"),
+            _INTL("Ought Not the Foxtrot to Double Aught of What We Sought?"),
             _INTL("Enemy Pokemon have all of their legal abilities!")
         )
         curses_array.push(curse_policy)
--- Plugins/Tectonic Battle/AI/Trainers AI/Curses/ExtraTypes.rb
+++ Plugins/Tectonic Battle/AI/Trainers AI/Curses/ExtraTypes.rb
@@ -1,7 +1,7 @@
 PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_EXTRA_TYPES,
     proc { |curse_policy, battle, curses_array|
         battle.amuletActivates(
-            _INTL("TODO"),
+            _INTL("A Portrait of the Opposition in Radiant Ulfire and Stygian Blue"),
             _INTL("Enemy Pokemon all have an extra type.")
         )
         curses_array.push(curse_policy)
