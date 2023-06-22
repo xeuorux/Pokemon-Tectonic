e86d8ba5db81edeb6d63f6058345533e9f0a2884
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-06-22 00:35:26 -0700

    URSHIFU fixes
    Fixed level up learnsets and form names
    When swapping forms, they lose their previous forms sig move and learn the new sig move

--- Plugins/Tectonic Battle/FormHandlers.rb
+++ Plugins/Tectonic Battle/FormHandlers.rb
@@ -69,3 +69,31 @@ MultipleForms.register(:SHAYMIN,{
     next 0 if pkmn.fainted? || pkmn.frozen? || PBDayNight.isNight?
   }
 })
+
+MultipleForms.register(:URSHIFU,{
+  "onSetForm" => proc { |pkmn, form, oldForm|
+    form_moves = %i[WICKEDBLOW SURGINGSTRIKES]
+
+    move_index = -1
+    pkmn.moves.each_with_index do |move, i|
+      next unless form_moves.any? { |m| m == move.id }
+      move_index = i
+      break
+    end
+
+    # Try learning that form's unique move
+    new_move_id = form_moves[form - 1]
+    if move_index >= 0
+      # Knows another form's unique move; replace it
+      old_move_name = pkmn.moves[move_index].name
+      pkmn.moves[move_index].id = new_move_id
+      new_move_name = pkmn.moves[move_index].name
+      pbMessage(_INTL("1,\\wt[16] 2, and\\wt[16]...\\wt[16] ...\\wt[16] ... Ta-da!\\se[Battle ball drop]\1"))
+      pbMessage(_INTL("{1} forgot how to use {2}.\\nAnd...\1", pkmn.name, old_move_name))
+      pbMessage(_INTL("\\se[]{1} learned {2}!\\se[Pkmn move learnt]", pkmn.name, new_move_name))
+    else
+      # Just try to learn this form's unique move
+      pbLearnMove(pkmn, new_move_id, true)
+    end
+  }
+})
