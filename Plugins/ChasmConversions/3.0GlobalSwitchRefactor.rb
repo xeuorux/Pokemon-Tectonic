52b638653acfbb481a498e1d1ab7a7045152d730
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-07-20 15:37:41 -0700

    Global switch refactor for gym leader avatar quest cutscene

---
+++ Plugins/ChasmConversions/3.0GlobalSwitchRefactor.rb
@@ -0,0 +1,54 @@
+
+SaveData.register_conversion(:global_switch_refactor_3_0) do
+  game_version '3.0.0'
+  display_title 'Setting global switches based on self-switches.'
+  to_all do |save_data|
+    globalSwitches = save_data[:switches]
+    selfSwitches = save_data[:self_switches]
+    itemBag = save_data[:bag]
+
+    globalSwitches[112] = selfSwitches[[57,11,'B']] # Growlithe rescued
+    echoln("Growlithe rescued: #{globalSwitches[112]}")
+
+    globalSwitches[113] = selfSwitches[[10,4,'A']] # Raikou defeated
+    globalSwitches[114] = selfSwitches[[10,6,'B']] # Raikou guards gone
+    globalSwitches[115] = selfSwitches[[125,1,'A']] # Entei defeated
+    globalSwitches[116] = selfSwitches[[125,6,'C']] # Entei guards gone
+    globalSwitches[117] = selfSwitches[[124,1,'A']] # Suicune defeated
+    globalSwitches[118] = selfSwitches[[124,6,'B']] # Suicune guards gone
+
+    for i in 1..3 do
+      echoln("Beast #{i} defeated / guards dismissed: #{globalSwitches[111 + i * 2]} / #{globalSwitches[112 + i * 2]}")
+    end
+
+    # Add a Rainbow Feather for each of the defeated legendary beasts
+    itemBag.pbStoreItem(:RAINBOWFEATHER, 1, false) if globalSwitches[113]
+    itemBag.pbStoreItem(:RAINBOWFEATHER, 1, false) if globalSwitches[115]
+    itemBag.pbStoreItem(:RAINBOWFEATHER, 1, false) if globalSwitches[117]
+    echoln("#{itemBag.pbQuantity(:RAINBOWFEATHER)} rainbow feathers earned")
+
+    # Avatar chambers
+    globalSwitches[126] = selfSwitches[[42,2,'A']] # Chamber 1 defeated
+    globalSwitches[127] = selfSwitches[[84,6,'A']] # Chamber 2 defeated
+    globalSwitches[128] = selfSwitches[[24,4,'A']] # Chamber 3 defeated
+    globalSwitches[129] = selfSwitches[[106,3,'A']] # Chamber 4 defeated
+    globalSwitches[130] = selfSwitches[[176,3,'A']] # Chamber 5 defeated
+    globalSwitches[131] = selfSwitches[[199,2,'A']] # Chamber 6 defeated
+    globalSwitches[132] = selfSwitches[[291,2,'A']] # Chamber 7 defeated
+    globalSwitches[133] = selfSwitches[[303,5,'A']] # Chamber 8 defeated
+
+    globalSwitches[134] = selfSwitches[[44,3,'A']] # Chamber 1 quested
+    globalSwitches[135] = selfSwitches[[62,17,'A']] # Chamber 2 quested
+    globalSwitches[136] = selfSwitches[[21,4,'A']] # Chamber 3 quested
+    globalSwitches[137] = selfSwitches[[104,5,'A']] # Chamber 4 quested
+    globalSwitches[138] = selfSwitches[[175,4,'A']] # Chamber 5 quested
+    globalSwitches[139] = selfSwitches[[195,15,'B']] # Chamber 6 quested
+    globalSwitches[140] = selfSwitches[[279,7,'A']] # Chamber 7 quested
+    globalSwitches[141] = selfSwitches[[300,1,'A']] # Chamber 8 quested
+
+    # Debug info
+    for i in 1..8 do
+      echoln("Chamber #{i} quested/defeated: #{globalSwitches[133 + i]} / #{globalSwitches[125 + i]}")
+    end
+  end
+end
