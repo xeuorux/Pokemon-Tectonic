2b23dbd384714674fab49b60ab710fb96ce0410b
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-08-02 22:18:33 -0700

    Create PokeBalls.rb

---
+++ Plugins/Tectonic Other/Items/Item Behaviours/PokeBalls.rb
@@ -0,0 +1,38 @@
+ItemHandlers::CanUseInBattle.addIf(proc { |item| GameData::Item.get(item).is_poke_ball? },   # Poké Balls
+  proc { |item,pokemon,battler,move,firstAction,battle,scene,showMessages|
+    if battle.pbPlayer.party_full? && $PokemonStorage.full?
+      scene.pbDisplay(_INTL("There is no room left in the PC!")) if showMessages
+      next false
+    end
+    # NOTE: Using a Poké Ball consumes all your actions for the round. The code
+    #       below is one half of making this happen; the other half is in def
+    #       pbItemUsesAllActions?.
+    if !firstAction
+      scene.pbDisplay(_INTL("It's impossible to aim without being focused!")) if showMessages
+      next false
+    end
+    if battler.semiInvulnerable?
+      scene.pbDisplay(_INTL("It's no good! It's impossible to aim at a Pokémon that's not in sight!")) if showMessages
+      next false
+    end
+    # NOTE: The code below stops you from throwing a Poké Ball if there is more
+    #       than one unfainted opposing Pokémon. (Snag Balls can be thrown in
+    #       this case, but only in trainer battles, and the trainer will deflect
+    #       them if they are trying to catch a non-Shadow Pokémon.)
+    if battle.pbOpposingBattlerCount>1 && !(GameData::Item.get(item).is_snag_ball? && battle.trainerBattle?)
+      if battle.pbOpposingBattlerCount==2
+        scene.pbDisplay(_INTL("It's no good! It's impossible to aim when there are two Pokémon!")) if showMessages
+      else
+        scene.pbDisplay(_INTL("It's no good! It's impossible to aim when there are more than one Pokémon!")) if showMessages
+      end
+      next false
+    end
+    next true
+  }
+)
+
+ItemHandlers::UseInBattle.addIf(proc { |item| GameData::Item.get(item).is_poke_ball? },   # Poké Balls
+  proc { |item,battler,battle|
+    battle.pbThrowPokeBall(battler.index,item)
+  }
+)
