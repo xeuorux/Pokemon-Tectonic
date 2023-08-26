4d79a8578f44e62a22ff50ee7ae99fa5efa44946
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-08-26 04:15:23 -0700

    FRIENDGUARD/MOONBLANKET fix

--- Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Damage/DamageCalcTargetAllyAbilities.rb
+++ Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Damage/DamageCalcTargetAllyAbilities.rb
@@ -1,12 +1,12 @@
 BattleHandlers::DamageCalcTargetAllyAbility.add(:FRIENDGUARD,
-  proc { |ability, _user, _target, _move, mults, _baseDmg, _type, aiCheck|
+  proc { |ability, _user, target, _move, mults, _baseDmg, _type, aiCheck|
       mults[:final_damage_multiplier] *= 0.75
       target.aiLearnsAbility(ability) unless aiCheck
   }
 )
 
 BattleHandlers::DamageCalcTargetAllyAbility.add(:MOONBLANKET,
-  proc { |ability, user, _target, _move, mults, _baseDmg, _type, aiCheck|
+  proc { |ability, user, target, _move, mults, _baseDmg, _type, aiCheck|
     if user.battle.moonGlowing?
       mults[:final_damage_multiplier] *= 0.75
       target.aiLearnsAbility(ability) unless aiCheck
