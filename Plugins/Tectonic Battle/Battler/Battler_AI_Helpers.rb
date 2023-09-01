602a015f2983188b44f53457b3aba92e60bed1a1
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-08-31 21:57:28 -0700

    Healing scoring adjustment

--- Plugins/Tectonic Battle/Battler/Battler_AI_Helpers.rb
+++ Plugins/Tectonic Battle/Battler/Battler_AI_Helpers.rb
@@ -31,7 +31,7 @@ class PokeBattle_Battler
         healingScore = 0
         healingScore += healingAmount * pbDefense
         healingScore += healingAmount * pbSpDef
-        healingScore /= (2 * level)
+        healingScore /= (3 * level)
         healingPercentage = (100 * healingAmount / @totalhp.to_f).round(1)
         echoln("\t\t[EFFECT SCORING] #{pbThis} scores the value of healing #{healingAmount} HP (#{healingPercentage} percent) at #{healingScore}")
         return healingScore
