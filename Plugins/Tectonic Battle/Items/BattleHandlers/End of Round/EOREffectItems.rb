be774cfe429757fcfeacc401bd7d6c46748e238e
Hocusgit <hocuspocusfakeusemailus@gmail.com>
2023-09-21 16:47:00 -0700

    Status Orb fix
    and by fix i mean they exist in the code now

--- Plugins/Tectonic Battle/Items/BattleHandlers/End of Round/EOREffectItems.rb
+++ Plugins/Tectonic Battle/Items/BattleHandlers/End of Round/EOREffectItems.rb
@@ -7,3 +7,27 @@ BattleHandlers::EOREffectItem.add(:STICKYBARB,
       battler.applyFractionalDamage(STICKY_BARB_DAMAGE_FRACTION)
   }
 )
+
+BattleHandlers::EOREffectItem.add(:POISONORB,
+  proc { |item, battler, battle|
+	  next unless battler.canPoison?(nil, false)
+      battler.applyPoison(nil,
+  	  _INTL("{1} was poisoned by the {2}! {3}!", battler.pbThis, getItemName(item), POISONED_EXPLANATION))
+  }
+)
+
+BattleHandlers::EOREffectItem.add(:FROSTORB,
+  proc { |item, battler, battle|
+	  next unless battler.canFrostbite?(nil, false)
+      battler.applyFrostbite(nil,
+  	  _INTL("{1} was frostbitten by the {2}! {3}!", battler.pbThis, getItemName(item), FROSTBITE_EXPLANATION))
+  }
+)
+
+BattleHandlers::EOREffectItem.add(:FLAMEORB,
+  proc { |item, battler, battle|
+	  next unless battler.canBurn?(nil, false)
+      battler.applyBurn(nil,
+  	  _INTL("{1} was burned by the {2}! {3}!", battler.pbThis, getItemName(item), BURNED_EXPLANATION))
+  }
+)
