57fd0d8021d2c11f6c02109b870c98f75f469554
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-10-18 10:59:31 -0700

    Hoopa quest QA and improvements

--- Plugins/Tectonic Other/Event Helpers/LegendaryFights.rb
+++ Plugins/Tectonic Other/Event Helpers/LegendaryFights.rb
@@ -1,6 +1,7 @@
 def legendaryFight(species, level, switch = 'A', form = 0)
     Pokemon.play_cry(species)
     level = [level,getLevelCap].min
+    pbWait(30)
     if pbWildBattleCore(species, level) == 4
         fadeSwitchOn(switch)
     else
