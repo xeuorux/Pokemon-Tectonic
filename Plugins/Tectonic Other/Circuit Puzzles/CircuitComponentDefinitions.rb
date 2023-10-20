843e8af753bbfd1ff7a91ca05bf3f6beab067d2d
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2023-10-20 09:19:21 -0700

    Circuit puzzle components definitions

--- Plugins/Tectonic Other/Circuit Puzzles/CircuitComponentDefinitions.rb
+++ Plugins/Tectonic Other/Circuit Puzzles/CircuitComponentDefinitions.rb
@@ -2,5 +2,29 @@ CIRCUIT_COMPONENTS = {
     :tswitch_left => {
         :graphic => "tswitch_left",
         :states => 2,
-    }
+    },
+    :tswitch_right => {
+        :graphic => "tswitch_right",
+        :states => 2,
+    },
+    :tswitch_up => {
+        :graphic => "tswitch_up",
+        :states => 2,
+    },
+    :tswitch_cross => {
+        :graphic => "tswitch_cross",
+        :states => 4,
+    },
+    :cross_switch => {
+        :graphic => "crossswitch",
+        :states => 2,
+    },
+    :ground_spinner => {
+        :graphic => "ground_spinner",
+        :states => 4,
+    },
+    :turntable => {
+        :graphic => "turntable",
+        :states => 4,
+    },
 }
