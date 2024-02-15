240f687c66d0cb6ff10067e2a5e6c917df97a8b0
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-02-14 18:11:51 -0700

    Graphics/Pictures cleanup and organization

--- Plugins/Tectonic Graphics and UI/Menus/Town Map/PokemonRegionMap_Scene.rb
+++ Plugins/Tectonic Graphics and UI/Menus/Town Map/PokemonRegionMap_Scene.rb
@@ -77,7 +77,7 @@ class PokemonRegionMap_Scene
               @sprites["map2"].y = @sprites["map"].y
             end
             pbDrawImagePositions(@sprites["map2"].bitmap,[
-               ["Graphics/Pictures/#{hidden[4]}",hidden[2]*SQUAREWIDTH,hidden[3]*SQUAREHEIGHT]
+               ["Graphics/Pictures/Map Extras/#{hidden[4]}",hidden[2]*SQUAREWIDTH,hidden[3]*SQUAREHEIGHT]
             ])
           end
         end
