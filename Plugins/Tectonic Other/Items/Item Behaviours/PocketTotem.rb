3d0669be66ef0aae7f0cd21f46b259b9b69fe494
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-02-24 12:55:43 -0700

    Fix inconsistencies with teleport blockages
    Also, no longer prevent TPing out from gyms

--- Plugins/Tectonic Other/Items/Item Behaviours/PocketTotem.rb
+++ Plugins/Tectonic Other/Items/Item Behaviours/PocketTotem.rb
@@ -1,31 +1,10 @@
 ItemHandlers::UseFromBag.add(:POCKETTOTEM,proc { |item|
-  if $game_player.pbHasDependentEvents?
-    pbMessage(_INTL("It can't be used when you have someone with you."))
-    next 0
-  end
-  if !GameData::MapMetadata.exists?($game_map.map_id)
-    next 0
-  end
-  if GameData::MapMetadata.get($game_map.map_id).teleport_blocked
-	  pbMessage(_INTL("You are prevented from teleporting due to an unknown force."))
-    next 0
-  end
-  next 2
+  next 2 if canTeleport?(true)
+  next 0
 })
 
 ItemHandlers::ConfirmUseInField.add(:POCKETTOTEM,proc { |item|
-  if $game_player.pbHasDependentEvents?
-    pbMessage(_INTL("It can't be used when you have someone with you."))
-    next false
-  end
-  if !GameData::MapMetadata.exists?($game_map.map_id)
-    next false
-  end
-  if GameData::MapMetadata.get($game_map.map_id).teleport_blocked
-	  pbMessage(_INTL("You are prevented from teleporting due to an unknown force."))
-    next false
-  end
-  next true
+  next canTeleport?(true)
 })
 
 ItemHandlers::UseInField.add(:POCKETTOTEM,proc { |item|
