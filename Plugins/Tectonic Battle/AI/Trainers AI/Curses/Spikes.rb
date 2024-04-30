b4b8edb729905d8e05afb9ddebeb82d5f570c2ab
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-04-30 15:32:31 -0700

    Helena's curse rework
    Sets 1 spikes at the beginning of each turn

--- Plugins/Tectonic Battle/AI/Trainers AI/Curses/Spikes.rb
+++ Plugins/Tectonic Battle/AI/Trainers AI/Curses/Spikes.rb
@@ -1,8 +1,16 @@
 PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_SPIKES,
     proc { |curse_policy, battle, curses_array|
-        battle.amuletActivates(_INTL("Gored Upon the Horns of the Dilemma - Boredom or the Path of Thorns?"))
-        battle.sides[0].incrementEffect(:Spikes, GameData::BattleEffect.get(:Spikes).maximum)
+        battle.amuletActivates(
+            _INTL("Gored Upon the Horns of the Dilemma - Boredom or the Path of Thorns?"),
+            _INTL("One layer of spikes will be placed on your side each turn!"),
+        )
         curses_array.push(curse_policy)
         next curses_array
+    }
+)
+
+PokeBattle_Battle::BeginningOfTurnCurseEffect.add(:CURSE_SPIKES,
+    proc { |curse_policy, battle|
+        battle.sides[0].incrementEffect(:Spikes)
     }
 )
