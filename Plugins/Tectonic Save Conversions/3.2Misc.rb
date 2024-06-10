a76060456f914c99a7b2c189269bedfe14f0bf94
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-06-10 12:20:45 -0700

    CELLBOOSTER distributed as Avatar of Zygarde reward
    added a save conversion to give it to people that already have the Zygarde Cube
    added the Zygarde Cube sitting on the ground in Zygarde's Chamber

---
+++ Plugins/Tectonic Save Conversions/3.2Misc.rb
@@ -0,0 +1,14 @@
+
+SaveData.register_conversion(:misc_fixes_3_2_0) do
+    game_version '3.2.0'
+    display_title 'Fixing misc. stuff'
+    to_all do |save_data|
+      globalSwitches = save_data[:switches]
+      globalVariables = save_data[:variables]
+      selfSwitches = save_data[:self_switches]
+      itemBag = save_data[:bag]
+  
+      itemBag.pbChangeItem(:ZYGARDECUBE,:CELLBOOSTER)
+    end
+  end
+  
