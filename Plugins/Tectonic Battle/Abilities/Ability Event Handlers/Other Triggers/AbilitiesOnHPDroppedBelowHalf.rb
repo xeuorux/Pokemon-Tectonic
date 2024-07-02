f669075e24c9a83e6530f6897695f312a6cf45ce
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-07-01 17:07:08 -0700

    EMERGENCYEXIT works the same in wild battles as in trainer battles

--- Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Other Triggers/AbilitiesOnHPDroppedBelowHalf.rb
+++ Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Other Triggers/AbilitiesOnHPDroppedBelowHalf.rb
@@ -1,18 +1,6 @@
 BattleHandlers::AbilityOnHPDroppedBelowHalf.add(:EMERGENCYEXIT,
     proc { |ability, battler, battle|
         next false if battler.fainted?
-        # In wild battles
-        if battle.wildBattle?
-            next false if battler.opposes? && battle.pbSideBattlerCount(battler.index) > 1
-            next false unless battle.pbCanRun?(battler.index)
-            battle.pbShowAbilitySplash(battler, ability, true)
-            battle.pbHideAbilitySplash(battler)
-            pbSEPlay("Battle flee")
-            battle.pbDisplay(_INTL("{1} fled from battle!", battler.pbThis))
-            battle.decision = 3 # Escaped
-            next true
-        end
-        # In trainer battles
         next false if battle.pbAllFainted?(battler.idxOpposingSide)
         next battle.triggeredSwitchOut(battler.index, ability: ability)
     }
