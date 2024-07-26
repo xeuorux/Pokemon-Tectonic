381bfc3c17b7fa937a12356dd1e7a4116553ea3f
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-07-26 08:34:52 -0700

    Added cutscene hinting at Swords of Justice moving into the Carnation Tower

---
+++ Plugins/Tectonic Content Scripts/SwordsOfJusticeCutscene.rb
@@ -0,0 +1,15 @@
+SWORDS_OF_JUSTICE_DEFEATED_VAR = 31
+
+def progressSwordsOfJusticeQuest
+    incrementGlobalVar(SWORDS_OF_JUSTICE_DEFEATED_VAR)
+    return unless getGlobalVariable(SWORDS_OF_JUSTICE_DEFEATED_VAR) == 3
+    currentBGM = $game_system.playing_bgm
+    pbBGMFade(1.0)
+    pbWait(Graphics.frame_rate)
+    pbMessage(_INTL("\\wmThe words of a poem appear in your mind."))
+    pbMessage(_INTL("\\ss\\wm\"Watchful eye, a towering sight.\""))
+    pbMessage(_INTL("\\ss\\wm\"Budding hope, with 3-fold might.\""))
+    pbMessage(_INTL("\\ss\\wm\"We await you, o slayer...\""))
+    pbWait(Graphics.frame_rate)
+    pbBGMPlay(currentBGM)
+end
