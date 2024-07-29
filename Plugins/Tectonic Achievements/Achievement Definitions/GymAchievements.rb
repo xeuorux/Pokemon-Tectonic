54b18a69ae9b7acefa05e28475e7e230653e6b70
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-07-29 14:11:09 -0700

    PERFECT_CURSED_GYM achievements fix

--- Plugins/Tectonic Achievements/Achievement Definitions/GymAchievements.rb
+++ Plugins/Tectonic Achievements/Achievement Definitions/GymAchievements.rb
@@ -18,7 +18,7 @@ def checkCursedGymPerfectAchievement(gymNumber)
     return unless battlePerfected?
     return unless tarotAmuletActive?
     return unless getLevelCap <= GYM_LEVEL_CAPS[gymNumber-1]
-    achievementID = ("PERFECT_CURSED_GYM_" + gymNumber).to_sym
+    achievementID = ("PERFECT_CURSED_GYM_" + gymNumber.to_s).to_sym
     unlockAchievement(achievementID)
 end
 
