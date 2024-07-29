b46ae8a54db59b0b9aca9567ad0b8eec9bd12b8b
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-07-29 08:35:33 -0700

    Add WIN_BATTLE_MONUMENT_ALL_SAME_GENERATION achievement

--- Plugins/Tectonic Achievements/Achievement Definitions/BattleMonumentAchievements.rb
+++ Plugins/Tectonic Achievements/Achievement Definitions/BattleMonumentAchievements.rb
@@ -1,10 +1,15 @@
 def checkBattleMonumentVictoryAchievements
     unlockAchievement(:WIN_BATTLE_MONUMENT)
-    checkBattleMonumentNoTribeOverlapVictoryAchievement
+    checkBattleMonumentAllSameGenerationAchievement
 end
 
 def checkBattleMonumentNoTribeOverlapVictoryAchievement
-    tribesPresent = []
     return if playerTribalBonus.hasAnyTribeOverlap?
-    unlockAchievement(:WIN_BATTLE_MONUMENT)
+    unlockAchievement(:WIN_BATTLE_MONUMENT_NO_TRIBE_OVERLAP)
+end
+
+def checkBattleMonumentAllSameGenerationAchievement
+    firstGeneration = $Trainer.party[0].species_data.generation
+    return if $Trainer.party.any? { |partyMember| partyMember.species_data.generation != firstGeneration }
+    unlockAchievement(:WIN_BATTLE_MONUMENT_ALL_SAME_GENERATION)
 end
