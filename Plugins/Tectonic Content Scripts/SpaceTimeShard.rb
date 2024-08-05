1b6d815e2d5617413addbe5c719afed97697119d
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-08-04 21:15:28 -0700

    Added and distributed SPACETIMESHARDs to prep for future Dialga/Palkia quest

--- Plugins/Tectonic Content Scripts/SpaceTimeShard.rb
+++ Plugins/Tectonic Content Scripts/SpaceTimeShard.rb
@@ -1,10 +1,41 @@
-# Map ID, Shard Event ID, TP Location Event ID
+# Map ID, TP Location Event ID, Shard Event ID
 SPACE_TIME_SHARD_LOCATIONS = [
-    [136,0,17]
+    [38,0,31], # Bluepoint Beach
+    [56,0,76], # Novo Town
+    [6,0,46], # LuxTech
+    [430,0,47], # Amber Hills
+    [55,0,39], # Lingering Delta
+    [78,0,7], # LuxTech Main
+    [301,0,44], # County Park
+    [7,0,45], # Repora Forest
+    [114,0,9], # Velenz Underground East
+    [396,0,9], # Crater-Shelf Gatehouse
+    [299,0,13], # Fin Center
+    [124,0,10], # Clearwater Cave
+    [37,0,30], # Svait
+    [185,0,64], # Eleig Stretch
+    [426,0,75], # Prizca Black Market
+    [212,0,59], # Ruins Digsite
+    [144,0,5], # Artist's House
+    [155,0,42], # Prizca West
+    [173,0,10], # Full Blast Records
+    [422,0,3], # Leo's Elite Items
+    [367,0,24], # Prizca Great Hall
+    [186,0,68], # Frostflow Farms
+    [187,0,110], # Prizca East
+    [288,0,9], # Underground River
+    [193,0,26], # Volcanic Shore
+    [183,0,4], # Circuit Cave
+    [313,0,4], # Wren's House
+    [333,0,22], # Floral Maze
+    [216,0,31], # Highland Lake
+    [211,0,49], # Split Peaks
+    [418,0,3], # M. Munna Den 3
+    [264,0,6], # Sweetrock Harbor Mart
 ]
 
 def collectedShard?(locationDetails)
-    return pbGetSelfSwitch(locationDetails[1],'A',locationDetails[0])
+    return pbGetSelfSwitch(locationDetails[2],'A',locationDetails[0])
 end
 
 def collectedAllShards?
@@ -37,7 +68,8 @@ end
 
 def teleportToRandomSpaceTimeShard
     raise _INTL("Can't warp to any shards because they're all collected already!") if collectedAllShards?
-    mapID, shardEventID, warpEventID = getRandomSpaceTimeShardLocation
+    mapID, warpEventID, shardEventID = getRandomSpaceTimeShardLocation
+    warpEventID = 1 # Until these events are actually created
     pbSEPlay("Anim/PRSFX- Roar of Time2")
     pbWait(30)
     teleportLeaveAnimation(false)
