2d53e91472ce5430852b4a76c54cbb3fdbe2d447
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-08-05 20:11:40 -0700

    Raised STORE_LOTS_OF_EXP exp threshold by a lot

--- Plugins/Tectonic Other/Globals/GlobalMetadata.rb
+++ Plugins/Tectonic Other/Globals/GlobalMetadata.rb
@@ -244,6 +244,6 @@ class PokemonGlobalMetadata
     
     def expJAR=(value)
         @expJAR = value
-        unlockAchievement(:STORE_LOTS_OF_EXP) if @expJAR >= 300_000
+        unlockAchievement(:STORE_LOTS_OF_EXP) if @expJAR >= 1_00_000
     end
 end
