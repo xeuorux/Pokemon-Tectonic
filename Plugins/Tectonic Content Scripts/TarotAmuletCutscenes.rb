cdb7127e68658a6fcf4639dcb3f8461a8d41b2f5
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-08-11 15:37:28 -0700

    Gyms 5,6,7,8 dialogue rework
    And tarot amulet cutscene code fixes

--- Plugins/Tectonic Content Scripts/TarotAmuletCutscenes.rb
+++ Plugins/Tectonic Content Scripts/TarotAmuletCutscenes.rb
@@ -6,10 +6,17 @@ TAROT_CUTSCENE_PLAYED_AT_GYM_GLOBAL = 359
 def checkTarotAmuletCutscene(gymIndex)
     return unless tarotAmuletActive?
     tarotCutScenePlayedGlobal = TAROT_CUTSCENE_PLAYED_AT_GYM_GLOBAL + (gymIndex - 1)
-    return if getGlobalSwitch(tarotCutScenePlayedGlobal)
-    tarotIndex = getGlobalVariable(TAROT_CUTSCENE_PROGRESS_GLOBAL) + gymIndex
-    globalIDToEnable = PLAY_TAROT_AMULET_CUTSCENE_GLOBAL + tarotIndex
+    if getGlobalSwitch(tarotCutScenePlayedGlobal)
+        echoln("Tarot Amulet cutscene already played for this gym.")
+        return
+    end
+    # Mark this gym as having been triggered already
     setGlobalSwitch(tarotCutScenePlayedGlobal)
+
+    # Start the next cutscene in the sequence
+    globalIDToEnable = PLAY_TAROT_AMULET_CUTSCENE_GLOBAL + getGlobalVariable(TAROT_CUTSCENE_PROGRESS_GLOBAL)
     setGlobalSwitch(globalIDToEnable)
+
+    # Progress the cutscene progress
     incrementGlobalVar(TAROT_CUTSCENE_PROGRESS_GLOBAL)
 end
