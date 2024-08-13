792bcc87b81f8c94a978d1c6ccf51a0eab9d5a61
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-08-12 19:38:44 -0700

    Vibrant Cave rework
    Vibrant cave rework
    Add Vibrant Cave as a second warp point from the Carnation Sigil
    Rework Vibrant Cave as the “Carnation Stockpile”, with ruins aesthetic both inside and out
    Remove encounters from the Carnation Stockpile by:
    Moving Larvitar back to the Hollowed Layer
    Moving Unown from the Hollowed Layer to a new encounter set in the ruins digsite
    Moving Joltik to LuxTech Campus
    Moving Larvesta to Repora Forest
    Moving one of the Repora Forest Ghost-types to Velenz
    
    Moved the overhead flying Corvisquires from Repora Forest to The Shelf

--- Plugins/Tectonic Other/Items/Item Behaviours/CarnationSigil.rb
+++ Plugins/Tectonic Other/Items/Item Behaviours/CarnationSigil.rb
@@ -1,14 +1,27 @@
 ItemHandlers::UseFromBag.add(:CARNATIONSIGIL,proc { |item|
     next 0 unless canTeleport?(true)
-    next pbConfirmMessageSerious(_INTL("You feel yourself pulled away. Allow yourself to be transported?")) ? 2 : 0
+    next pbMessage(_INTL("You feel yourself being pulled away.")) ? 2 : 0
 })
 
 ItemHandlers::ConfirmUseInField.add(:CARNATIONSIGIL,proc { |item|
   next false unless canTeleport?(true)
-  next pbConfirmMessageSerious(_INTL("You feel yourself pulled away. Allow yourself to be transported?"))
+  next pbMessage(_INTL("You feel yourself being pulled away."))
 })
 
 ItemHandlers::UseInField.add(:CARNATIONSIGIL,proc { |item|
-  transferPlayerToEvent(30,Down,186) # An event placed on top of the carnation tower in frostflow farms
-  next 1
+    commands = []
+    commands.push(_INTL("The Tower"))
+    commands.push(_INTL("The Stockpile"))
+    commands.push(_INTL("Cancel"))
+    choiceNumber = pbMessage(_INTL("Where would you like to go?"),commands,commands.length)
+    case choiceNumber
+    when 0
+        transferPlayerToEvent(30,Down,186)
+        next 1
+    when 1
+        transferPlayerToEvent(13,Down,126)
+        next 1
+    when 2
+        next 0
+    end
 })
