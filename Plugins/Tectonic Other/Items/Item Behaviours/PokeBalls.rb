9d06cfb43bbc51cc770c7dd77a92e2a2028990d9
Xeuorux <67340902+xeuorux@users.noreply.github.com>
2024-09-03 00:22:07 -0700

    Can catch Pokemon even when multiple wild battlers active

--- Plugins/Tectonic Other/Items/Item Behaviours/PokeBalls.rb
+++ Plugins/Tectonic Other/Items/Item Behaviours/PokeBalls.rb
@@ -19,14 +19,14 @@ ItemHandlers::CanUseInBattle.addIf(proc { |item| GameData::Item.get(item).is_pok
     #       than one unfainted opposing Pokémon. (Snag Balls can be thrown in
     #       this case, but only in trainer battles, and the trainer will deflect
     #       them if they are trying to catch a non-Shadow Pokémon.)
-    if battle.pbOpposingBattlerCount>1
-      if battle.pbOpposingBattlerCount==2
-        pbSceneDefaultDisplay(_INTL("It's no good! It's impossible to aim when there are two Pokémon!"),scene) if showMessages
-      else
-        pbSceneDefaultDisplay(_INTL("It's no good! It's impossible to aim when there are more than one Pokémon!"),scene) if showMessages
-      end
-      next false
-    end
+    # if battle.pbOpposingBattlerCount>1
+    #   if battle.pbOpposingBattlerCount==2
+    #     pbSceneDefaultDisplay(_INTL("It's no good! It's impossible to aim when there are two Pokémon!"),scene) if showMessages
+    #   else
+    #     pbSceneDefaultDisplay(_INTL("It's no good! It's impossible to aim when there are more than one Pokémon!"),scene) if showMessages
+    #   end
+    #   next false
+    # end
     next true
   }
 )
