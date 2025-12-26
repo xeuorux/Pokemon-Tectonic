9d2602087ae77c8cd11c116f958a1fbad6bcf9f4
ZinniaGameDev <xeuorux@gmail.com>
2025-12-25 22:26:44 -0700

    Break Through ability tweaked: Also boosts move damage by 20%

--- Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Damage/DamageCalcUserAbilities.rb
+++ Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Damage/DamageCalcUserAbilities.rb
@@ -970,3 +970,10 @@ BattleHandlers::DamageCalcUserAbility.add(:SLUMBERFORCE,
     end
   }
 )
+
+BattleHandlers::DamageCalcUserAbility.add(:BREAKTHROUGH,
+  proc { |ability, user, target, move, mults, _baseDmg, type, aiCheck|
+    mults[:attack_multiplier] *= 1.2
+    user.aiLearnsAbility(ability) unless aiCheck
+  }
+)
